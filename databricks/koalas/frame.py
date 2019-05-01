36eb08b8a30d9fc3c0b50844f5f695fb53f51995
Shril Kumar <shril.iitdhn@gmail.com>
2019-05-01 17:07:09 +0530

    Inline documentation of isnull and and notnull functions

--- databricks/koalas/frame.py
+++ databricks/koalas/frame.py
@@ -309,7 +309,7 @@ class DataFrame(_Frame):
         """
         Detects missing values for items in the current Dataframe.
 
-        Return a boolean same-sized Dataframe indicating if the values are NA. 
+        Return a boolean same-sized Dataframe indicating if the values are NA.
         NA values, such as None or numpy.NaN, gets mapped to True values.
         Everything else gets mapped to False values.
 
