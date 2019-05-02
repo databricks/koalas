182fa02b19e230e3dd7a916f5f83d1a9782ef9f1
Shril Kumar <shril.iitdhn@gmail.com>
2019-05-02 23:53:07 +0530

    Added inline documentation for iteritems

--- databricks/koalas/frame.py
+++ databricks/koalas/frame.py
@@ -163,8 +163,46 @@ class DataFrame(_Frame):
         """
         return corr(self, method)
 
-    @derived_from(pd.DataFrame)
     def iteritems(self):
+        """
+        Iterator over (column name, Series) pairs.
+
+        Iterates over the DataFrame columns, returning a tuple with
+        the column name and the content as a Series.
+        
+        Yields
+        ------
+        label : object
+            The column names for the DataFrame being iterated over.
+        content : Series
+            The column entries belonging to each label, as a Series.
+
+        Examples
+        --------
+        >>> df = ks.DataFrame({'species': ['bear', 'bear', 'marsupial'],
+        ...                    'population': [1864, 22000, 80000]},
+        ...                     index=['panda', 'polar', 'koala'])
+        >>> df
+                 species  population                                                    
+        panda       bear        1864
+        polar       bear       22000
+        koala  marsupial       80000
+
+        >>> for label, content in df.iteritems():
+        ...    print('label:', label)
+        ...    print('content:', content.to_string(), sep='\n')
+        ... 
+        label: species
+        content:
+        panda         bear
+        polar         bear
+        koala    marsupial
+        label: population
+        content:
+        panda     1864
+        polar    22000
+        koala    80000
+        """
         cols = list(self.columns)
         return list((col_name, self[col_name]) for col_name in cols)
 
