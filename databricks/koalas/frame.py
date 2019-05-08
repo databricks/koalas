691275491efdce70777225fe5e401c50a30f2db8
Reynold Xin <rxin@databricks.com>
2019-05-07 18:52:58 -0700

    Make sort_values deterministic

--- databricks/koalas/frame.py
+++ databricks/koalas/frame.py
@@ -1305,33 +1305,45 @@ defaultdict(<class 'list'>, {'col..., 'col...})]
         Examples
         --------
         >>> df = ks.DataFrame({
-        ...     'col1': ['A', 'A', 'B', None, 'D', 'C'],
-        ...     'col2': [2, 1, 9, 8, 7, 4],
-        ...     'col3': [0, 1, 9, 4, 2, 3],
+        ...     'col1': ['A', 'B', None, 'D', 'C'],
+        ...     'col2': [2, 9, 8, 7, 4],
+        ...     'col3': [0, 9, 4, 2, 3],
         ... })
         >>> df
            col1  col2  col3
         0     A     2     0
-        1     A     1     1
-        2     B     9     9
-        3  None     8     4
-        4     D     7     2
-        5     C     4     3
+        1     B     9     9
+        2  None     8     4
+        3     D     7     2
+        4     C     4     3
 
         Sort by col1
 
         >>> df.sort_values(by=['col1'])
            col1  col2  col3
         0     A     2     0
-        1     A     1     1
-        2     B     9     9
-        5     C     4     3
-        4     D     7     2
-        3  None     8     4
+        1     B     9     9
+        4     C     4     3
+        3     D     7     2
+        2  None     8     4
 
+        Sort Descending
+
+        >>> df.sort_values(by='col1', ascending=False)
+           col1  col2  col3
+        3     D     7     2
+        4     C     4     3
+        1     B     9     9
+        0     A     2     0
+        2  None     8     4
 
         Sort by multiple columns
 
+        >>> df = ks.DataFrame({
+        ...     'col1': ['A', 'A', 'B', None, 'D', 'C'],
+        ...     'col2': [2, 1, 9, 8, 7, 4],
+        ...     'col3': [0, 1, 9, 4, 2, 3],
+        ... })
         >>> df.sort_values(by=['col1', 'col2'])
            col1  col2  col3
         1     A     1     1
@@ -1339,17 +1351,6 @@ defaultdict(<class 'list'>, {'col..., 'col...})]
         2     B     9     9
         5     C     4     3
         4     D     7     2
-        3  None     8     4
-
-        Sort Descending
-
-        >>> df.sort_values(by='col1', ascending=False)
-           col1  col2  col3
-        4     D     7     2
-        5     C     4     3
-        2     B     9     9
-        0     A     2     0
-        1     A     1     1
         3  None     8     4
         """
         if isinstance(by, string_types):
