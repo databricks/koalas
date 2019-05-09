24b8fd1bf813ab8b796878ca601be604e5527744
Shril Kumar <shril.iitdhn@gmail.com>
2019-05-09 12:18:00 +0530

    Inline doc for DataFrame.assign (#267)

--- databricks/koalas/frame.py
+++ databricks/koalas/frame.py
@@ -798,8 +798,62 @@ defaultdict(<class 'list'>, {'col..., 'col...})]
     # Alias to maintain backward compatibility with Spark
     toPandas = to_pandas
 
-    @derived_from(pd.DataFrame)
     def assign(self, **kwargs):
+        """
+        Assign new columns to a DataFrame.
+
+        Returns a new object with all original columns in addition to new ones.
+        Existing columns that are re-assigned will be overwritten.
+
+        Parameters
+        ----------
+        **kwargs : dict of {str: callable or Series}
+            The column names are keywords. If the values are
+            callable, they are computed on the DataFrame and
+            assigned to the new columns. The callable must not
+            change input DataFrame (though koalas doesn't check it).
+            If the values are not callable, (e.g. a Series or a literal),
+            they are simply assigned.
+
+        Returns
+        -------
+        DataFrame
+            A new DataFrame with the new columns in addition to
+            all the existing columns.
+
+        Examples
+        --------
+        >>> df = ks.DataFrame({'temp_c': [17.0, 25.0]},
+        ...                   index=['Portland', 'Berkeley'])
+        >>> df
+                  temp_c
+        Portland    17.0
+        Berkeley    25.0
+
+        Where the value is a callable, evaluated on `df`:
+
+        >>> df.assign(temp_f=lambda x: x.temp_c * 9 / 5 + 32)
+                  temp_c  temp_f
+        Portland    17.0    62.6
+        Berkeley    25.0    77.0
+
+        Alternatively, the same behavior can be achieved by directly
+        referencing an existing Series or sequence and you can also
+        create multiple columns within the same assign.
+
+        >>> df.assign(temp_f=df['temp_c'] * 9 / 5 + 32,
+        ...           temp_k=df['temp_c'] + 273.15)
+                  temp_c  temp_f  temp_k
+        Portland    17.0    62.6  290.15
+        Berkeley    25.0    77.0  298.15
+
+        Notes
+        -----
+        Assigning multiple columns within the same ``assign`` is possible
+        but you cannot refer to newly created or modified columns. This
+        feature is supported in Pandas for Python 3.6 and later but not in
+        Koalas. In koalas, all items are computed first, and then assigned.
+        """
         from databricks.koalas.series import Series
         for k, v in kwargs.items():
             if not (isinstance(v, (Series, spark.Column)) or
