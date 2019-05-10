c18d5f995ffc41cf7030fd2057e8ca04c2dd10d3
garawalid <gwalid94@gmail.com>
2019-05-10 23:00:55 +0200

    Fix doc

--- databricks/koalas/series.py
+++ databricks/koalas/series.py
@@ -516,8 +516,8 @@ class Series(_Frame):
         Write a text representation of object to the system clipboard.
         This can be pasted into Excel, for example.
 
-            .. note:: This method should only be used if the resulting DataFrame is expected
-                to be small, as all the data is loaded into the driver's memory.
+        .. note:: This method should only be used if the resulting DataFrame is expected
+            to be small, as all the data is loaded into the driver's memory.
 
         Parameters
         ----------
