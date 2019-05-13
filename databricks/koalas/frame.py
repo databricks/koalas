57b7acb1c512c891e02c5c96f219aa396d5abe6e
Reynold Xin <rxin@databricks.com>
2019-05-12 23:47:06 -0700

    Various doc updates

--- databricks/koalas/frame.py
+++ databricks/koalas/frame.py
@@ -1588,15 +1588,21 @@ defaultdict(<class 'list'>, {'col..., 'col...})]
     def fillna(self, value=None, axis=None, inplace=False):
         """Fill NA/NaN values.
 
-        :param value: scalar, dict, Series
-                    Value to use to fill holes. alternately a dict/Series of values
-                    specifying which value to use for each column.
-                    DataFrame is not supported.
-        :param axis: {0 or `index`}
-                    1 and `columns` are not supported.
-        :param inplace: boolean, default False
-                    Fill in place (do not create a new object)
-        :return: :class:`DataFrame`
+        Parameters
+        ----------
+        value : scalar, dict, Series
+            Value to use to fill holes. alternately a dict/Series of values
+            specifying which value to use for each column.
+            DataFrame is not supported.
+        axis : {0 or `index`}
+            1 and `columns` are not supported.
+        inplace : boolean, default False
+            Fill in place (do not create a new object)
+
+        Returns
+        -------
+        DataFrame
+            DataFrame with NA entries filled.
 
         Examples
         --------
@@ -2029,6 +2035,7 @@ defaultdict(<class 'list'>, {'col..., 'col...})]
            The sequence of values to test. If values is a dict,
            the keys must be the column names, which must match.
            Series and DataFrame are not supported.
+
         Returns
         -------
         DataFrame
