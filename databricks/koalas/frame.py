45254c007780a2356d6409eaec5e72932dec5433
Reynold Xin <rxin@databricks.com>
2019-05-12 22:02:38 -0700

    make tests deterministic in Python 3.5

--- databricks/koalas/frame.py
+++ databricks/koalas/frame.py
@@ -903,7 +903,8 @@ defaultdict(<class 'list'>, {'col..., 'col...})]
         --------
         >>> df = ks.DataFrame({'month': [1, 4, 7, 10],
         ...                    'year': [2012, 2014, 2013, 2014],
-        ...                    'sale': [55, 40, 84, 31]})
+        ...                    'sale': [55, 40, 84, 31]},
+        ...                   columns=['month', 'year', 'sale'])
         >>> df
            month  year  sale
         0      1  2012    55
