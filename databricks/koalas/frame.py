ee616de8dba3010da24518695366c1c788a55c40
Tim Hunter <timhunter@databricks.com>
2019-05-19 23:19:17 -0700

    changes

--- databricks/koalas/frame.py
+++ databricks/koalas/frame.py
@@ -1112,11 +1112,6 @@ defaultdict(<class 'list'>, {'col..., 'col...})]
             kdf.columns = columns
             return kdf
 
-    def astype(self):
-        pass
-
-    def cli
-
     def isnull(self):
         """
         Detects missing values for items in the current Dataframe.
