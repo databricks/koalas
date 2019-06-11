8fcb2092df0ab2a97e34a9408ce6ee378eb9eefc
nitlev <nitlev@users.noreply.github.com>
2019-06-11 13:05:26 +0200

    Stats on numeric columns (#422)
    Solves #280 
    
    Ensure stats functions other than min/max/count are applied on numeric/boolean columns only by default.

--- databricks/koalas/frame.py
+++ databricks/koalas/frame.py
@@ -274,13 +274,17 @@ class DataFrame(_Frame):
         self._internal = self._internal.copy(data_columns=metadata.data_columns,
                                              index_map=metadata.index_map)
 
-    def _reduce_for_stat_function(self, sfun):
+    def _reduce_for_stat_function(self, sfun, numeric_only=False):
         """
         Applies sfun to each column and returns a pd.Series where the number of rows equal the
         number of columns.
 
-        :param sfun: either an 1-arg function that takes a Column and returns a Column, or
+        Parameters
+        ----------
+        sfun : either an 1-arg function that takes a Column and returns a Column, or
         a 2-arg function that takes a Column and its DataType and returns a Column.
+        numeric_only : boolean, default False
+            If True, sfun is applied on numeric columns (including booleans) only.
         """
         from inspect import signature
         exprs = []
@@ -288,19 +292,25 @@ class DataFrame(_Frame):
         for col in self.columns:
             col_sdf = self._sdf[col]
             col_type = self._sdf.schema[col].dataType
-            if isinstance(col_type, BooleanType) and sfun.__name__ not in ('min', 'max'):
-                # Stat functions cannot be used with boolean values by default
-                # Thus, cast to integer (true to 1 and false to 0)
-                # Exclude the min and max methods though since those work with booleans
-                col_sdf = col_sdf.cast('integer')
-            if num_args == 1:
-                # Only pass in the column if sfun accepts only one arg
-                col_sdf = sfun(col_sdf)
-            else:  # must be 2
-                assert num_args == 2
-                # Pass in both the column and its data type if sfun accepts two args
-                col_sdf = sfun(col_sdf, col_type)
-            exprs.append(col_sdf.alias(col))
+
+            is_numeric_or_boolean = isinstance(col_type, (NumericType, BooleanType))
+            min_or_max = sfun.__name__ in ('min', 'max')
+            keep_column = not numeric_only or is_numeric_or_boolean or min_or_max
+
+            if keep_column:
+                if isinstance(col_type, BooleanType) and not min_or_max:
+                    # Stat functions cannot be used with boolean values by default
+                    # Thus, cast to integer (true to 1 and false to 0)
+                    # Exclude the min and max methods though since those work with booleans
+                    col_sdf = col_sdf.cast('integer')
+                if num_args == 1:
+                    # Only pass in the column if sfun accepts only one arg
+                    col_sdf = sfun(col_sdf)
+                else:  # must be 2
+                    assert num_args == 2
+                    # Pass in both the column and its data type if sfun accepts two args
+                    col_sdf = sfun(col_sdf, col_type)
+                exprs.append(col_sdf.alias(col))
 
         sdf = self._sdf.select(*exprs)
         pdf = sdf.toPandas()
@@ -2346,7 +2356,7 @@ defaultdict(<class 'list'>, {'col..., 'col...})]
         Single    5
         dtype: int64
         """
-        return self._reduce_for_stat_function(_Frame._count_expr)
+        return self._reduce_for_stat_function(_Frame._count_expr, numeric_only=False)
 
     def drop(self, labels=None, axis=1, columns: Union[str, List[str]] = None):
         """
