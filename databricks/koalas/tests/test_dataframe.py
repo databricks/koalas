a5428fe397118dc86985749c4000253b0451a996
[yuwillrun] <[6234127@gmail.com]>
2019-06-12 10:37:49 +0800

    pull update merge

--- databricks/koalas/tests/test_dataframe.py
+++ databricks/koalas/tests/test_dataframe.py
@@ -721,18 +721,27 @@ class DataFrameTest(ReusedSQLTestCase, SQLTestUtils):
         self.assert_eq(pdf.add_suffix('_col'), kdf.add_suffix('_col'))
 
     def test_join(self):
-        pdf1 = pd.DataFrame({'key': ['K0', 'K1', 'K2', 'K3', 'K4', 'K5'],
-                            'A': ['A0', 'A1', 'A2', 'A3', 'A4', 'A5']}, columns=['key', 'A'])
+        # check basic function
+        pdf1 = pd.DataFrame({'key': ['K0', 'K1', 'K2', 'K3'],
+                            'A': ['A0', 'A1', 'A2', 'A3']}, columns=['key', 'A'])
         pdf2 = pd.DataFrame({'key': ['K0', 'K1', 'K2'],
                              'B': ['B0', 'B1', 'B2']}, columns=['key', 'B'])
-        kdf1 = ks.DataFrame({'key': ['K0', 'K1', 'K2', 'K3', 'K4', 'K5'],
-                             'A': ['A0', 'A1', 'A2', 'A3', 'A4', 'A5']}, columns=['key', 'A'])
+        kdf1 = ks.DataFrame({'key': ['K0', 'K1', 'K2', 'K3'],
+                             'A': ['A0', 'A1', 'A2', 'A3']}, columns=['key', 'A'])
         kdf2 = ks.DataFrame({'key': ['K0', 'K1', 'K2'],
                              'B': ['B0', 'B1', 'B2']}, columns=['key', 'B'])
-        join_pdf = pdf1.join(pdf2, lsuffix='_caller', rsuffix='_other')
+        join_pdf = pdf1.join(pdf2, lsuffix='_left', rsuffix='_right')
         join_pdf.sort_values(by=list(join_pdf.columns), inplace=True)
 
-        join_kdf = kdf1.join(kdf2, lsuffix='_caller', rsuffix='_other')
+        join_kdf = kdf1.join(kdf2, lsuffix='_left', rsuffix='_right')
         join_kdf.sort_values(by=list(join_kdf.columns), inplace=True)
 
+        self.assert_eq(join_pdf, join_kdf)
+
+        # check `on` parameter
+        join_pdf = pdf1.join(pdf2.set_index('key'), on='key', lsuffix='_left', rsuffix='_right')
+        join_pdf.sort_values(by=list(join_pdf.columns), inplace=True)
+
+        join_kdf = kdf1.join(kdf2.set_index('key'), on='key', lsuffix='_left', rsuffix='_right')
+        join_kdf.sort_values(by=list(join_kdf.columns), inplace=True)
         self.assert_eq(join_pdf, join_kdf)
