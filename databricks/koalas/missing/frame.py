a87a824defc1c79e8b240bbb8b231274b8213f68
Rain Feng <6234127@gmail.com>
2019-06-25 17:33:29 +0800

    DataFrame.update() (#498)
    This PR adds DataFrame.update

--- databricks/koalas/missing/frame.py
+++ databricks/koalas/missing/frame.py
@@ -151,7 +151,6 @@ class _MissingPandasLikeDataFrame(object):
     tz_convert = unsupported_function('tz_convert')
     tz_localize = unsupported_function('tz_localize')
     unstack = unsupported_function('unstack')
-    update = unsupported_function('update')
     where = unsupported_function('where')
     xs = unsupported_function('xs')
 
