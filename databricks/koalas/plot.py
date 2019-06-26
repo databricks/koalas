fad3771615827e939d1f0770c1d6e8699c1f3b23
Hyukjin Kwon <gurwls223@apache.org>
2019-06-26 21:55:53 +0900

    Clean up plot - add license, inline the documentation, cleanup imports #509
    This PR cleans up `plot.py`

--- databricks/koalas/plot.py
+++ databricks/koalas/plot.py
@@ -1,16 +1,31 @@
-from databricks.koalas.missing import _unsupported_function
+#
+# Copyright (C) 2019 Databricks, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 import matplotlib
-from matplotlib.axes._base import _process_plot_format
 import numpy as np
 import pandas as pd
-from pandas.core.generic import _shared_docs
+
+from matplotlib.axes._base import _process_plot_format
 from pandas.io.formats.printing import pprint_thing
 from pandas.plotting._core import (
-    _all_kinds, _dataframe_kinds, _get_standard_kind, _gca, _series_kinds,
-    _shared_doc_series_kwargs, BarPlot, BasePlotMethods, BoxPlot, HistPlot, is_list_like,
-    is_integer, MPLPlot, string_types
-)
-from pandas.util._decorators import Appender
+    _all_kinds, _get_standard_kind, _gca, BarPlot, BasePlotMethods, BoxPlot, HistPlot,
+    is_list_like,
+    is_integer, MPLPlot)
+
+from databricks.koalas.missing import _unsupported_function
 from pyspark.ml.feature import Bucketizer
 from pyspark.sql import functions as F
 
@@ -395,7 +410,6 @@ _klasses = [KoalasHistPlot, KoalasBarPlot, KoalasBoxPlot]
 _plot_klass = {getattr(klass, '_kind'): klass for klass in _klasses}
 
 
-@Appender(_shared_docs['plot'] % _shared_doc_series_kwargs)
 def plot_series(data, kind='line', ax=None,                    # Series unique
                 figsize=None, use_index=True, title=None, grid=None,
                 legend=False, style=None, logx=False, logy=False, loglog=False,
@@ -404,6 +418,97 @@ def plot_series(data, kind='line', ax=None,                    # Series unique
                 yerr=None, xerr=None,
                 label=None, secondary_y=False,                 # Series unique
                 **kwds):
+    """
+    Make plots of Series using matplotlib / pylab.
+
+    Each plot kind has a corresponding method on the
+    ``Series.plot`` accessor:
+    ``s.plot(kind='line')`` is equivalent to
+    ``s.plot.line()``.
+
+    Parameters
+    ----------
+    data : Series
+
+    kind : str
+        - 'line' : line plot (default)
+        - 'bar' : vertical bar plot
+        - 'barh' : horizontal bar plot
+        - 'hist' : histogram
+        - 'box' : boxplot
+        - 'kde' : Kernel Density Estimation plot
+        - 'density' : same as 'kde'
+        - 'area' : area plot
+        - 'pie' : pie plot
+
+    ax : matplotlib axes object
+        If not passed, uses gca()
+    figsize : a tuple (width, height) in inches
+    use_index : boolean, default True
+        Use index as ticks for x axis
+    title : string or list
+        Title to use for the plot. If a string is passed, print the string at
+        the top of the figure. If a list is passed and `subplots` is True,
+        print each item in the list above the corresponding subplot.
+    grid : boolean, default None (matlab style default)
+        Axis grid lines
+    legend : False/True/'reverse'
+        Place legend on axis subplots
+    style : list or dict
+        matplotlib line style per column
+    logx : boolean, default False
+        Use log scaling on x axis
+    logy : boolean, default False
+        Use log scaling on y axis
+    loglog : boolean, default False
+        Use log scaling on both x and y axes
+    xticks : sequence
+        Values to use for the xticks
+    yticks : sequence
+        Values to use for the yticks
+    xlim : 2-tuple/list
+    ylim : 2-tuple/list
+    rot : int, default None
+        Rotation for ticks (xticks for vertical, yticks for horizontal plots)
+    fontsize : int, default None
+        Font size for xticks and yticks
+    colormap : str or matplotlib colormap object, default None
+        Colormap to select colors from. If string, load colormap with that name
+        from matplotlib.
+    colorbar : boolean, optional
+        If True, plot colorbar (only relevant for 'scatter' and 'hexbin' plots)
+    position : float
+        Specify relative alignments for bar plot layout.
+        From 0 (left/bottom-end) to 1 (right/top-end). Default is 0.5 (center)
+    table : boolean, Series or DataFrame, default False
+        If True, draw a table using the data in the DataFrame and the data will
+        be transposed to meet matplotlib's default layout.
+        If a Series or DataFrame is passed, use passed data to draw a table.
+    yerr : DataFrame, Series, array-like, dict and str
+        See :ref:`Plotting with Error Bars <visualization.errorbars>` for
+        detail.
+    xerr : same types as yerr.
+    label : label argument to provide to plot
+    secondary_y : boolean or sequence of ints, default False
+        If True then y-axis will be on the right
+    mark_right : boolean, default True
+        When using a secondary_y axis, automatically mark the column
+        labels with "(right)" in the legend
+    `**kwds` : keywords
+        Options to pass to matplotlib plotting method
+
+    Returns
+    -------
+    axes : :class:`matplotlib.axes.Axes` or numpy.ndarray of them
+
+    Notes
+    -----
+
+    - See matplotlib documentation online for more on this subject
+    - If `kind` = 'bar' or 'barh', you can specify relative alignments
+      for bar plot layout by `position` keyword.
+      From 0 (left/bottom-end) to 1 (right/top-end). Default is 0.5 (center)
+    """
 
     # function copied from pandas.plotting._core
     # so it calls modified _plot below
