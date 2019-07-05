9fe450291d7f0048067e70264f87fcfacdeac31a
fengyurun <fengyurun1@jd.com>
2019-07-06 00:07:23 +0800

    first version

--- databricks/koalas/frame.py
+++ databricks/koalas/frame.py
@@ -5123,6 +5123,71 @@ defaultdict(<class 'list'>, {'col..., 'col...})]
 
         return DataFrame(exploded_df)
 
+    # TODO: check decimals.index.is_unique
+    def round(self, decimals=0):
+        """
+        Round a DataFrame to a variable number of decimal places.
+
+        Parameters
+        ----------
+        decimals : int, dict, Series
+            Number of decimal places to round each column to. If an int is
+            given, round each column to the same number of places.
+            Otherwise dict and Series round to variable numbers of places.
+            Column names should be in the keys if `decimals` is a
+            dict-like, or in the index if `decimals` is a Series. Any
+            columns not included in `decimals` will be left as is. Elements
+            of `decimals` which are not columns of the input will be
+            ignored.
+
+        Returns
+        -------
+        DataFrame
+
+        See Also
+        --------
+        numpy.around
+        Series.round
+
+        Examples
+        --------
+        >>> df = ks.DataFrame(np.random.random([3, 3]),
+        ...     columns=['A', 'B', 'C'], index=['first', 'second', 'third'])
+        >>> df
+                       A         B         C
+        first   0.028208  0.992815  0.173891
+        second  0.038683  0.645646  0.577595
+        third   0.877076  0.149370  0.491027
+
+        >>> df.round(2)
+                   A     B     C
+        first   0.03  0.99  0.17
+        second  0.04  0.65  0.58
+        third   0.88  0.15  0.49
+
+        >>> df.round({'A': 1, 'C': 2})
+                  A         B     C
+        first   0.0  0.992815  0.17
+        second  0.0  0.645646  0.58
+        third   0.9  0.149370  0.49
+
+        >>> decimals = ks.Series([1, 0, 2], index=['A', 'B', 'C'])
+        >>> df.round(decimals)
+                  A  B     C
+        first   0.0  1  0.17
+        second  0.0  1  0.58
+        third   0.9  0  0.49
+        """
+        if isinstance(decimals, (dict, ks.Series)):
+            if isinstance(decimals, ks.Series):
+                if not decimals.index.is_unique:
+                    raise ValueError("Index of decimals must be unique")
+            new_cols = [col for col in _dict_round(self, decimals)]
+        elif isinstance(decimals):
+            new_cols = [_series_round(v, decimals) for _, v in self.iteritems()]
+        else:
+            raise TypeError("decimals must be an integer, a dict-like or a Series")
+
     def _pd_getitem(self, key):
         from databricks.koalas.series import Series
         if key is None:
