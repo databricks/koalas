5f8721bc9469543a2614e697d2fe01e0de37de5b
Hyukjin Kwon <gurwls223@apache.org>
2019-07-09 13:47:43 +0900

    Add DataFrame.transform (#541)
    This PR adds transform API in DataFrame.

--- databricks/koalas/frame.py
+++ databricks/koalas/frame.py
@@ -20,6 +20,7 @@ A wrapper class for Spark DataFrame to behave similar to pandas DataFrame.
 from distutils.version import LooseVersion
 import re
 import warnings
+import inspect
 from functools import partial, reduce
 import sys
 from typing import Any, Optional, List, Tuple, Union, Generic, TypeVar
@@ -1477,6 +1478,65 @@ defaultdict(<class 'list'>, {'col..., 'col...})]
         return DataFrame(internal)
 
     T = property(transpose)
+
+    def transform(self, func):
+        """
+        Call ``func`` on self producing a Series with transformed values
+        and that has the same length as its input.
+
+        .. note:: unlike pandas, it is required for ``func`` to specify return type hint.
+
+        .. note:: note that the series within ``func`` is actually a pandas series, and
+            the length of each series is not guaranteed.
+
+        Parameters
+        ----------
+        func : function
+            Function to use for transforming the data. It must work when pandas Series
+            is passed.
+
+        Returns
+        -------
+        DataFrame
+            A DataFrame that must have the same length as self.
+
+        Raises
+        ------
+        Exception : If the returned DataFrame has a different length than self.
+
+        Examples
+        --------
+        >>> df = ks.DataFrame({'A': range(3), 'B': range(1, 4)})
+        >>> df
+           A  B
+        0  0  1
+        1  1  2
+        2  2  3
+
+        >>> def square(x) -> ks.Series[np.int32]:
+        ...     return x ** 2
+        >>> df.transform(square)
+           A  B
+        0  0  1
+        1  1  4
+        2  4  9
+        """
+        assert callable(func), "the first argument should be a callable function."
+        spec = inspect.getfullargspec(func)
+        return_sig = spec.annotations.get("return", None)
+        if return_sig is None:
+            raise ValueError("Given function must have return type hint; however, not found.")
+
+        wrapped = ks.pandas_wraps(func)
+        applied = []
+        for column in self._internal.data_columns:
+            applied.append(wrapped(self[column]).rename(column))
+
+        sdf = self._sdf.select(
+            self._internal.index_columns + [c._scol for c in applied])
+        internal = self._internal.copy(sdf=sdf)
+
+        return DataFrame(internal)
 
     @property
     def index(self):
