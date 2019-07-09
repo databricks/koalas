4a4211b19c8b31d5a8433f70bb4742796870772d
HyukjinKwon <gurwls223@apache.org>
2019-07-09 22:22:50 +0900

    Add more message

--- databricks/koalas/generic.py
+++ databricks/koalas/generic.py
@@ -297,7 +297,8 @@ class _Frame(object):
         def cumprod(scol):
             @pandas_udf(returnType=self._kdf._sdf.schema[self.name].dataType)
             def negative_check(s):
-                assert ((s > 0) | (s.isnull())).all(), "values should be bigger than 0."
+                assert len(s) == 0 or ((s > 0) | (s.isnull())).all(), \
+                    "values should be bigger than 0: %s" % s
                 return s
 
             return F.sum(F.log(negative_check(scol)))
