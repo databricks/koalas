98b3abb2d304d7165f49b953409a5d1b5e95f5c5
HyukjinKwon <gurwls223@apache.org>
2019-07-10 10:51:10 +0900

    debug

--- databricks/koalas/tests/test_series.py
+++ databricks/koalas/tests/test_series.py
@@ -480,9 +480,8 @@ class SeriesTest(ReusedSQLTestCase, SQLTestUtils):
         self.assertEqual(repr(pser.cumprod()), repr(kser.cumprod()))
         self.assertEqual(repr(pser.cumprod(skipna=False)), repr(kser.cumprod(skipna=False)))
 
-        with self.assertRaisesRegex(Exception, "values should be bigger than 0"):
-            a = repr(koalas.Series([0, 1]).cumprod())
-            assert False, a
+        a = repr(koalas.Series([0, 1]).cumprod())
+        assert False, a
 
     def test_median(self):
         with self.assertRaisesRegex(ValueError, "accuracy must be an integer; however"):
