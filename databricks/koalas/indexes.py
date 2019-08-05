e0b2f506f5c88868b7eb7ca03f02da53bece1c1f
HyukjinKwon <gurwls223@apache.org>
2019-08-05 21:08:27 +0900

    Add size in Index

--- databricks/koalas/indexes.py
+++ databricks/koalas/indexes.py
@@ -75,6 +75,21 @@ class Index(IndexOpsMixin):
         """
         return Index(self._kdf, scol)
 
+    @property
+    def size(self) -> int:
+        """
+        Return an int representing the number of elements in this object.
+
+        Examples
+        --------
+        >>> df = ks.DataFrame([(.2, .3), (.0, .6), (.6, .0), (.2, .1)],
+        ...                   columns=['dogs', 'cats'],
+        ...                   index=list('abcd'))
+        >>> df.index.size
+        4
+        """
+        return len(self._kdf)  # type: ignore
+
     def to_pandas(self) -> pd.Index:
         """
         Return a pandas Series.
@@ -260,6 +275,21 @@ class MultiIndex(Index):
 
     def all(self, *args, **kwargs):
         raise TypeError("cannot perform all with this index type: MultiIndex")
+
+    @property
+    def size(self) -> int:
+        """
+        Return an int representing the number of elements in this object.
+
+        Examples
+        --------
+        >>> df = ks.DataFrame([(.2, .3), (.0, .6), (.6, .0), (.2, .1)],
+        ...                   columns=['dogs', 'cats'],
+        ...                   index=list('abcd'))
+        >>> df.set_index('dogs', append=True).index.size
+        4
+        """
+        return len(self._kdf)  # type: ignore
 
     @property
     def name(self) -> str:
