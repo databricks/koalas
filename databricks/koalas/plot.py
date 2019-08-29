7ae5f26735eed4fabb7d481d85e4594a157084e5
Kaiqi <kaiqi@kth.se>
2019-08-29 12:29:13 +0200

    remove docstring

--- databricks/koalas/plot.py
+++ databricks/koalas/plot.py
@@ -1119,21 +1119,8 @@ class KoalasFramePlotMethods(PandasObject):
             The column name or column position to be used as vertical
             coordinates for each point.
         s : scalar or array_like, optional
-            The size of each point. Possible values are:
-            - A single scalar so all points have the same size.
-            - A sequence of scalars, which will be used for each point's size
-              recursively. For instance, when passing [2, 14] all points size
-              will be either 2 or 14, alternatively.
         c : str, int or array_like, optional
-            The color of each point. Possible values are:
-            - A single color string referred to by name, RGB or RGBA code,
-              for instance 'red' or '#a98d19'.
-            - A sequence of color strings referred to by name, RGB or RGBA
-              code, which will be used for each point's color recursively. For
-              instance ['green', 'yellow'] all points will be filled in green or
-              yellow, alternatively.
-            - A column name or position whose values will be used to color the
-              marker points according to a colormap.
+
         **kwds: Optional
             Keyword arguments to pass on to :meth:`databricks.koalas.DataFrame.plot`.
 
