cef32499186b27c4a7c98ee1f134c87d586a0e4c
Kaiqi <kaiqi@kth.se>
2019-09-08 22:14:35 +0200

    Reformat usage logging

--- databricks/koalas/usage_logging/__init__.py
+++ databricks/koalas/usage_logging/__init__.py
@@ -30,10 +30,11 @@ from databricks.koalas.datetimes import DatetimeMethods
 from databricks.koalas.groupby import DataFrameGroupBy, SeriesGroupBy
 from databricks.koalas.indexes import Index, MultiIndex
 from databricks.koalas.missing.frame import _MissingPandasLikeDataFrame
-from databricks.koalas.missing.groupby import _MissingPandasLikeDataFrameGroupBy, \
-    _MissingPandasLikeSeriesGroupBy
-from databricks.koalas.missing.indexes import _MissingPandasLikeIndex, \
-    _MissingPandasLikeMultiIndex
+from databricks.koalas.missing.groupby import (
+    _MissingPandasLikeDataFrameGroupBy,
+    _MissingPandasLikeSeriesGroupBy,
+)
+from databricks.koalas.missing.indexes import _MissingPandasLikeIndex, _MissingPandasLikeMultiIndex
 from databricks.koalas.missing.series import _MissingPandasLikeSeries
 from databricks.koalas.series import Series
 from databricks.koalas.strings import StringMethods
@@ -56,14 +57,23 @@ def attach(logger_module: Union[str, ModuleType]) -> None:
     if isinstance(logger_module, str):
         logger_module = importlib.import_module(logger_module)
 
-    logger = getattr(logger_module, 'get_logger')()
+    logger = getattr(logger_module, "get_logger")()
 
     modules = [config, namespace]
-    classes = [DataFrame, Series, Index, MultiIndex,
-               DataFrameGroupBy, SeriesGroupBy, DatetimeMethods, StringMethods]
+    classes = [
+        DataFrame,
+        Series,
+        Index,
+        MultiIndex,
+        DataFrameGroupBy,
+        SeriesGroupBy,
+        DatetimeMethods,
+        StringMethods,
+    ]
 
     try:
         from databricks.koalas import mlflow
+
         modules.append(mlflow)
         classes.append(mlflow.PythonModelWrapper)
     except ImportError:
@@ -74,39 +84,53 @@ def attach(logger_module: Union[str, ModuleType]) -> None:
 
     # Modules
     for target_module in modules:
-        target_name = target_module.__name__.split('.')[-1]
-        for name in getattr(target_module, '__all__'):
+        target_name = target_module.__name__.split(".")[-1]
+        for name in getattr(target_module, "__all__"):
             func = getattr(target_module, name)
             if not inspect.isfunction(func):
                 continue
             setattr(target_module, name, _wrap_function(target_name, name, func, logger))
 
-    special_functions = set(['__init__', '__repr__', '__str__', '_repr_html_', '__len__',
-                             '__getitem__', '__setitem__', '__getattr__'])
+    special_functions = set(
+        [
+            "__init__",
+            "__repr__",
+            "__str__",
+            "_repr_html_",
+            "__len__",
+            "__getitem__",
+            "__setitem__",
+            "__getattr__",
+        ]
+    )
 
     # Classes
     for target_class in classes:
         for name, func in inspect.getmembers(target_class, inspect.isfunction):
-            if name.startswith('_') and name not in special_functions:
+            if name.startswith("_") and name not in special_functions:
                 continue
             setattr(target_class, name, _wrap_function(target_class.__name__, name, func, logger))
 
         for name, prop in inspect.getmembers(target_class, lambda o: isinstance(o, property)):
-            if name.startswith('_'):
+            if name.startswith("_"):
                 continue
             setattr(target_class, name, _wrap_property(target_class.__name__, name, prop, logger))
 
     # Missings
-    for original, missing in \
-        [(pd.DataFrame, _MissingPandasLikeDataFrame),
-         (pd.Series, _MissingPandasLikeSeries),
-         (pd.Index, _MissingPandasLikeIndex),
-         (pd.MultiIndex, _MissingPandasLikeMultiIndex),
-         (pd.core.groupby.DataFrameGroupBy, _MissingPandasLikeDataFrameGroupBy),
-         (pd.core.groupby.SeriesGroupBy, _MissingPandasLikeSeriesGroupBy)]:
+    for original, missing in [
+        (pd.DataFrame, _MissingPandasLikeDataFrame),
+        (pd.Series, _MissingPandasLikeSeries),
+        (pd.Index, _MissingPandasLikeIndex),
+        (pd.MultiIndex, _MissingPandasLikeMultiIndex),
+        (pd.core.groupby.DataFrameGroupBy, _MissingPandasLikeDataFrameGroupBy),
+        (pd.core.groupby.SeriesGroupBy, _MissingPandasLikeSeriesGroupBy),
+    ]:
         for name, func in inspect.getmembers(missing, inspect.isfunction):
-            setattr(missing, name,
-                    _wrap_missing_function(original.__name__, name, func, original, logger))
+            setattr(
+                missing,
+                name,
+                _wrap_missing_function(original.__name__, name, func, original, logger),
+            )
 
         for name, prop in inspect.getmembers(missing, lambda o: isinstance(o, property)):
             setattr(missing, name, _wrap_missing_property(original.__name__, name, prop, logger))
@@ -121,7 +145,7 @@ def _wrap_function(class_name, function_name, func, logger):
 
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        if hasattr(_local, 'logging') and _local.logging:
+        if hasattr(_local, "logging") and _local.logging:
             # no need to log since this should be internal call.
             return func(*args, **kwargs)
         _local.logging = True
@@ -130,11 +154,13 @@ def _wrap_function(class_name, function_name, func, logger):
             try:
                 res = func(*args, **kwargs)
                 logger.log_success(
-                    class_name, function_name, time.perf_counter() - start, signature)
+                    class_name, function_name, time.perf_counter() - start, signature
+                )
                 return res
             except Exception as ex:
                 logger.log_failure(
-                    class_name, function_name, ex, time.perf_counter() - start, signature)
+                    class_name, function_name, ex, time.perf_counter() - start, signature
+                )
                 raise
         finally:
             _local.logging = False
@@ -143,10 +169,9 @@ def _wrap_function(class_name, function_name, func, logger):
 
 
 def _wrap_property(class_name, property_name, prop, logger):
-
     @property
     def wrapper(self):
-        if hasattr(_local, 'logging') and _local.logging:
+        if hasattr(_local, "logging") and _local.logging:
             # no need to log since this should be internal call.
             return prop.fget(self)
         _local.logging = True
@@ -154,12 +179,10 @@ def _wrap_property(class_name, property_name, prop, logger):
             start = time.perf_counter()
             try:
                 res = prop.fget(self)
-                logger.log_success(
-                    class_name, property_name, time.perf_counter() - start)
+                logger.log_success(class_name, property_name, time.perf_counter() - start)
                 return res
             except Exception as ex:
-                logger.log_failure(
-                    class_name, property_name, ex, time.perf_counter() - start)
+                logger.log_failure(class_name, property_name, ex, time.perf_counter() - start)
                 raise
         finally:
             _local.logging = False
@@ -177,7 +200,7 @@ def _wrap_missing_function(class_name, function_name, func, original, logger):
 
     signature = inspect.signature(getattr(original, function_name))
 
-    is_deprecated = func.__name__ == 'deprecated_function'
+    is_deprecated = func.__name__ == "deprecated_function"
 
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
@@ -191,7 +214,7 @@ def _wrap_missing_function(class_name, function_name, func, original, logger):
 
 def _wrap_missing_property(class_name, property_name, prop, logger):
 
-    is_deprecated = prop.fget.__name__ == 'deprecated_property'
+    is_deprecated = prop.fget.__name__ == "deprecated_property"
 
     @property
     def wrapper(self):
