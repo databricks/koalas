5e476ed8731aebdcf9148e257d1b51810cab2b87
Hyukjin Kwon <gurwls223@apache.org>
2019-09-10 09:24:34 +0900

    Use spark.write.csv in to_csv of Series and DataFrame (#749)
    This PR proposes to use `spark.write.csv` API to enable distributed computation when `path` is specified. If `path` is not specified, it just calls pandas' `to_csv` as was.
    
    Closes #677

--- databricks/koalas/tests/test_csv.py
+++ databricks/koalas/tests/test_csv.py
@@ -13,13 +13,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
+import os
+import shutil
+import tempfile
 from contextlib import contextmanager
-from distutils.version import LooseVersion
 
 import pandas as pd
+import numpy as np
 
-from databricks import koalas
+from databricks import koalas as ks
 from databricks.koalas.testing.utils import ReusedSQLTestCase, TestUtils
 
 
@@ -28,6 +30,12 @@ def normalize_text(s):
 
 
 class CsvTest(ReusedSQLTestCase, TestUtils):
+
+    def setUp(self):
+        self.tmp_dir = tempfile.mkdtemp(prefix=CsvTest.__name__)
+
+    def tearDown(self):
+        shutil.rmtree(self.tmp_dir, ignore_errors=True)
 
     @property
     def csv_text(self):
@@ -84,7 +92,7 @@ class CsvTest(ReusedSQLTestCase, TestUtils):
 
             def check(header='infer', names=None, usecols=None):
                 expected = pd.read_csv(fn, header=header, names=names, usecols=usecols)
-                actual = koalas.read_csv(fn, header=header, names=names, usecols=usecols)
+                actual = ks.read_csv(fn, header=header, names=names, usecols=usecols)
                 self.assertPandasAlmostEqual(expected, actual.toPandas())
 
             check()
@@ -96,62 +104,120 @@ class CsvTest(ReusedSQLTestCase, TestUtils):
             check(usecols=[1, 0])
             check(usecols=['amount'])
             check(usecols=['amount', 'name'])
-            if LooseVersion("0.20.0") <= LooseVersion(pd.__version__):
-                check(usecols=lambda x: x == 'amount')
             check(usecols=[])
             check(usecols=[1, 1])
             check(usecols=['amount', 'amount'])
-            if LooseVersion("0.20.0") <= LooseVersion(pd.__version__):
-                check(usecols=lambda x: x == 'a')
             check(names=['n', 'a'], usecols=['a'])
 
             # check with pyspark patch.
             expected = pd.read_csv(fn)
-            actual = koalas.read_csv(fn)
+            actual = ks.read_csv(fn)
             self.assertPandasAlmostEqual(expected, actual.toPandas())
 
             self.assertRaisesRegex(ValueError, 'non-unique',
-                                   lambda: koalas.read_csv(fn, names=['n', 'n']))
+                                   lambda: ks.read_csv(fn, names=['n', 'n']))
             self.assertRaisesRegex(ValueError, 'does not match the number.*3',
-                                   lambda: koalas.read_csv(fn, names=['n', 'a', 'b']))
+                                   lambda: ks.read_csv(fn, names=['n', 'a', 'b']))
             self.assertRaisesRegex(ValueError, 'does not match the number.*3',
-                                   lambda: koalas.read_csv(fn, header=0, names=['n', 'a', 'b']))
+                                   lambda: ks.read_csv(fn, header=0, names=['n', 'a', 'b']))
             self.assertRaisesRegex(ValueError, 'Usecols do not match.*3',
-                                   lambda: koalas.read_csv(fn, usecols=[1, 3]))
+                                   lambda: ks.read_csv(fn, usecols=[1, 3]))
             self.assertRaisesRegex(ValueError, 'Usecols do not match.*col',
-                                   lambda: koalas.read_csv(fn, usecols=['amount', 'col']))
+                                   lambda: ks.read_csv(fn, usecols=['amount', 'col']))
             self.assertRaisesRegex(ValueError, 'Unknown header argument 1',
-                                   lambda: koalas.read_csv(fn, header='1'))
+                                   lambda: ks.read_csv(fn, header='1'))
             expected_error_message = ("'usecols' must either be list-like of all strings, "
                                       "all unicode, all integers or a callable.")
             self.assertRaisesRegex(ValueError, expected_error_message,
-                                   lambda: koalas.read_csv(fn, usecols=[1, 'amount']))
+                                   lambda: ks.read_csv(fn, usecols=[1, 'amount']))
 
     def test_read_with_spark_schema(self):
         with self.csv_file(self.csv_text_2) as fn:
-            actual = koalas.read_csv(fn, names="A string, B string, C long, D long, E long")
+            actual = ks.read_csv(fn, names="A string, B string, C long, D long, E long")
             expected = pd.read_csv(fn, names=['A', 'B', 'C', 'D', 'E'])
             self.assertEqual(repr(expected), repr(actual))
 
     def test_read_csv_with_comment(self):
         with self.csv_file(self.csv_text_with_comments) as fn:
             expected = pd.read_csv(fn, comment='#')
-            actual = koalas.read_csv(fn, comment='#')
+            actual = ks.read_csv(fn, comment='#')
             self.assertPandasAlmostEqual(expected, actual.toPandas())
 
             self.assertRaisesRegex(ValueError, 'Only length-1 comment characters supported',
-                                   lambda: koalas.read_csv(fn, comment='').show())
+                                   lambda: ks.read_csv(fn, comment='').show())
             self.assertRaisesRegex(ValueError, 'Only length-1 comment characters supported',
-                                   lambda: koalas.read_csv(fn, comment='##').show())
+                                   lambda: ks.read_csv(fn, comment='##').show())
             self.assertRaisesRegex(ValueError, 'Only length-1 comment characters supported',
-                                   lambda: koalas.read_csv(fn, comment=1))
+                                   lambda: ks.read_csv(fn, comment=1))
             self.assertRaisesRegex(ValueError, 'Only length-1 comment characters supported',
-                                   lambda: koalas.read_csv(fn, comment=[1]))
+                                   lambda: ks.read_csv(fn, comment=[1]))
 
     def test_read_csv_with_mangle_dupe_cols(self):
         self.assertRaisesRegex(ValueError, 'mangle_dupe_cols',
-                               lambda: koalas.read_csv('path', mangle_dupe_cols=False))
+                               lambda: ks.read_csv('path', mangle_dupe_cols=False))
 
     def test_read_csv_with_parse_dates(self):
         self.assertRaisesRegex(ValueError, 'parse_dates',
-                               lambda: koalas.read_csv('path', parse_dates=True))
+                               lambda: ks.read_csv('path', parse_dates=True))
+
+    def test_to_csv(self):
+        pdf = pd.DataFrame({'a': [1, 2, 3], 'b': [4, 5, 6]}, index=[0, 1, 3])
+        kdf = ks.DataFrame(pdf)
+
+        self.assert_eq(kdf.to_csv(), pdf.to_csv(index=False))
+
+        pdf = pd.DataFrame({
+            'a': [1, np.nan, 3],
+            'b': ["one", "two", None],
+        }, index=[0, 1, 3])
+
+        kdf = ks.from_pandas(pdf)
+
+        self.assert_eq(kdf.to_csv(na_rep='null'), pdf.to_csv(na_rep='null', index=False))
+
+        pdf = pd.DataFrame({
+            'a': [1.0, 2.0, 3.0],
+            'b': [4.0, 5.0, 6.0],
+        }, index=[0, 1, 3])
+
+        kdf = ks.from_pandas(pdf)
+
+        self.assert_eq(kdf.to_csv(), pdf.to_csv(index=False))
+        self.assert_eq(kdf.to_csv(header=False), pdf.to_csv(header=False, index=False))
+        self.assert_eq(kdf.to_csv(), pdf.to_csv(index=False))
+
+    def test_to_csv_with_path(self):
+        pdf = pd.DataFrame({'a': [1, 2, 3], 'b': ['a', 'b', 'c']})
+        kdf = ks.DataFrame(pdf)
+
+        kdf.to_csv(self.tmp_dir, num_files=1)
+        expected = pdf.to_csv(index=False)
+
+        output_paths = [path for path in os.listdir(self.tmp_dir) if path.startswith("part-")]
+        assert len(output_paths) > 0
+        output_path = "%s/%s" % (self.tmp_dir, output_paths[0])
+        self.assertEqual(open(output_path).read(), expected)
+
+    def test_to_csv_with_path_and_basic_options(self):
+        pdf = pd.DataFrame({'a': [1, 2, 3], 'b': ['a', 'b', 'c']})
+        kdf = ks.DataFrame(pdf)
+
+        kdf.to_csv(self.tmp_dir, num_files=1, sep='|', header=False)
+        expected = pdf.to_csv(index=False, sep='|', header=False)
+
+        output_paths = [path for path in os.listdir(self.tmp_dir) if path.startswith("part-")]
+        assert len(output_paths) > 0
+        output_path = "%s/%s" % (self.tmp_dir, output_paths[0])
+        self.assertEqual(open(output_path).read(), expected)
+
+    def test_to_csv_with_path_and_pyspark_options(self):
+        pdf = pd.DataFrame({'a': [1, 2, 3, None], 'b': ['a', 'b', 'c', None]})
+        kdf = ks.DataFrame(pdf)
+
+        kdf.to_csv(self.tmp_dir, nullValue="null", num_files=1)
+        expected = pdf.to_csv(index=False, na_rep='null')
+
+        output_paths = [path for path in os.listdir(self.tmp_dir) if path.startswith("part-")]
+        assert len(output_paths) > 0
+        output_path = "%s/%s" % (self.tmp_dir, output_paths[0])
+        self.assertEqual(open(output_path).read(), expected)
