45b1bf5ddb0db38f3ef1d769cd2156453808f902
HaeJoon Lee <44108233+itholic@users.noreply.github.com>
2019-09-13 16:30:17 +0900

    Add axis parameter to dataframe.diff (#774)
    Since pandas dataframe support axis parameter in dataframe.diff.
    
    <img width="772" alt="스크린샷 2019-09-12 오후 1 47 32" src="https://user-images.githubusercontent.com/44108233/64754921-30949300-d564-11e9-8ae6-8f7e52f18951.png">
    
    I think we need to support it either. (And it was already commented as TODO)
    
    In addition, now the nunique function only support the axis parameter as 0 (without 'index'), also fix it.
    
    <img width="922" alt="스크린샷 2019-09-12 오후 1 53 41" src="https://user-images.githubusercontent.com/44108233/64755104-fb3c7500-d564-11e9-83cb-93226ab8b4b7.png">
    
    
    And currently we only support axis 0, or axis 1 for some functions, so i plan to implement it soon.

--- databricks/koalas/frame.py
+++ databricks/koalas/frame.py
@@ -2192,8 +2192,8 @@ defaultdict(<class 'list'>, {'col..., 'col...})]
         internal = self._internal.copy(sdf=sdf, data_columns=[c.name for c in applied])
         return DataFrame(internal)
 
-    # TODO: add axis parameter
-    def diff(self, periods=1):
+    # TODO: axis should support 1 or 'columns' either at this moment
+    def diff(self, periods: int = 1, axis: Union[int, str] = 0):
         """
         First discrete difference of element.
 
@@ -2209,6 +2209,8 @@ defaultdict(<class 'list'>, {'col..., 'col...})]
         ----------
         periods : int, default 1
             Periods to shift for calculating difference, accepts negative values.
+        axis : int, default 0 or 'index'
+            Can only be set to 0 at the moment.
 
         Returns
         -------
@@ -2259,6 +2261,8 @@ defaultdict(<class 'list'>, {'col..., 'col...})]
         4 -1.0 -3.0 -11.0
         5  NaN  NaN   NaN
         """
+        if axis not in [0, 'index']:
+            raise ValueError('axis should be either 0 or "index" currently.')
         applied = []
         for column in self._internal.data_columns:
             applied.append(self[column].diff(periods))
@@ -2267,7 +2271,8 @@ defaultdict(<class 'list'>, {'col..., 'col...})]
         internal = self._internal.copy(sdf=sdf, data_columns=[c.name for c in applied])
         return DataFrame(internal)
 
-    def nunique(self, axis: int = 0, dropna: bool = True, approx: bool = False,
+    # TODO: axis should support 1 or 'columns' either at this moment
+    def nunique(self, axis: Union[int, str] = 0, dropna: bool = True, approx: bool = False,
                 rsd: float = 0.05) -> pd.Series:
         """
         Return number of unique elements in the object.
@@ -2276,7 +2281,7 @@ defaultdict(<class 'list'>, {'col..., 'col...})]
 
         Parameters
         ----------
-        axis : int, default 0
+        axis : int, default 0 or 'index'
             Can only be set to 0 at the moment.
         dropna : bool, default True
             Don’t include NaN in the count.
@@ -2314,8 +2319,8 @@ defaultdict(<class 'list'>, {'col..., 'col...})]
         B    1
         Name: 0, dtype: int64
         """
-        if axis != 0:
-            raise ValueError("The 'nunique' method only works with axis=0 at the moment")
+        if axis not in [0, 'index']:
+            raise ValueError('axis should be either 0 or "index" currently.')
         res = self._sdf.select([self[column]._nunique(dropna, approx, rsd)
                                 for column in self.columns])
         return res.toPandas().T.iloc[:, 0]
