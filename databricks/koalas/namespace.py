ebe5a774ba254b02adcc2aac147d5f017101fceb
itholic <haejoon309@naver.com>
2019-09-14 15:30:10 +0900

    Add '_get_index_map' func to remove duplication

--- databricks/koalas/namespace.py
+++ databricks/koalas/namespace.py
@@ -123,8 +123,8 @@ def range(start: int,
     return DataFrame(sdf)
 
 
-def read_csv(path, header='infer', names=None, usecols=None,
-             mangle_dupe_cols=True, parse_dates=False, comment=None, index_col=None):
+def read_csv(path, header='infer', names=None, index_col=None,
+             usecols=None, mangle_dupe_cols=True, parse_dates=False, comment=None):
     """Read CSV (comma-separated) file into DataFrame.
 
     Parameters
@@ -241,16 +241,7 @@ def read_csv(path, header='infer', names=None, usecols=None,
     else:
         sdf = default_session().createDataFrame([], schema=StructType())
 
-    index_map = None  # type: Optional[List[IndexMap]]
-
-    if index_col is not None:
-        if isinstance(index_col, str):
-            index_col = [index_col]
-        sdf_columns = set(sdf.columns)
-        for col in index_col:
-            if col not in sdf_columns:
-                raise KeyError(col)
-        index_map = [(col, col) for col in index_col]
+    index_map = _get_index_map(sdf, index_col)  # type: Optional[List[IndexMap]]
 
     return DataFrame(_InternalFrame(sdf=sdf, index_map=index_map))
 
@@ -334,16 +325,7 @@ def read_table(name: str, index_col: Optional[Union[str, List[str]]] = None) -> 
     0   0
     """
     sdf = default_session().read.table(name)
-    index_map = None  # type: Optional[List[IndexMap]]
-
-    if index_col is not None:
-        if isinstance(index_col, str):
-            index_col = [index_col]
-        sdf_columns = set(sdf.columns)
-        for col in index_col:
-            if col not in sdf_columns:
-                raise KeyError(col)
-        index_map = [(col, col) for col in index_col]
+    index_map = _get_index_map(sdf, index_col)  # type: Optional[List[IndexMap]]
 
     return DataFrame(_InternalFrame(sdf=sdf, index_map=index_map))
 
@@ -391,16 +373,7 @@ def read_spark_io(path: Optional[str] = None, format: Optional[str] = None,
     0   0
     """
     sdf = default_session().read.load(path=path, format=format, schema=schema, options=options)
-    index_map = None  # type: Optional[List[IndexMap]]
-
-    if index_col is not None:
-        if isinstance(index_col, str):
-            index_col = [index_col]
-        sdf_columns = set(sdf.columns)
-        for col in index_col:
-            if col not in sdf_columns:
-                raise KeyError(col)
-        index_map = [(col, col) for col in index_col]
+    index_map = _get_index_map(sdf, index_col)  # type: Optional[List[IndexMap]]
 
     return DataFrame(_InternalFrame(sdf=sdf, index_map=index_map))
 
@@ -449,16 +422,8 @@ def read_parquet(path, columns=None, index_col=None) -> DataFrame:
     else:
         sdf = default_session().createDataFrame([], schema=StructType())
 
-    if index_col is not None:
-        if isinstance(index_col, str):
-            index_col = [index_col]
-        sdf_columns = set(sdf.columns)
-        for col in index_col:
-            if col not in sdf_columns:
-                raise KeyError(col)
-        index_map = [(col, col) for col in index_col]  # type: Optional[List[IndexMap]]
-    else:
-        index_map = None
+    index_map = _get_index_map(sdf, index_col)  # type: Optional[List[IndexMap]]
+
     return DataFrame(_InternalFrame(sdf=sdf, index_map=index_map))
 
 
@@ -859,16 +824,7 @@ def read_sql_table(table_name, con, schema=None, index_col=None, columns=None, *
         reader.schema(schema)
     reader.options(**options)
     sdf = reader.format("jdbc").load()
-    if index_col is not None:
-        if isinstance(index_col, str):
-            index_col = [index_col]
-        sdf_columns = set(sdf.columns)
-        for col in index_col:
-            if col not in sdf_columns:
-                raise KeyError(col)
-        index_map = [(col, col) for col in index_col]
-    else:
-        index_map = None
+    index_map = _get_index_map(sdf, index_col)  # type: Optional[List[IndexMap]]
     kdf = DataFrame(_InternalFrame(sdf=sdf, index_map=index_map))
     if columns is not None:
         if isinstance(columns, str):
@@ -919,16 +875,7 @@ def read_sql_query(sql, con, index_col=None, **options):
     reader.option('url', con)
     reader.options(**options)
     sdf = reader.format("jdbc").load()
-    if index_col is not None:
-        if isinstance(index_col, str):
-            index_col = [index_col]
-        sdf_columns = set(sdf.columns)
-        for col in index_col:
-            if col not in sdf_columns:
-                raise KeyError(col)
-        index_map = [(col, col) for col in index_col]
-    else:
-        index_map = None
+    index_map = _get_index_map(sdf, index_col)  # type: Optional[List[IndexMap]]
     return DataFrame(_InternalFrame(sdf=sdf, index_map=index_map))
 
 
@@ -1678,6 +1625,23 @@ def _to_datetime2(arg_year, arg_month, arg_day,
         errors=errors,
         format=format,
         infer_datetime_format=infer_datetime_format)
+
+    index_map = None  # type: Optional[List[IndexMap]]
+
+
+def _get_index_map(sdf, index_col=None):
+    if index_col is not None:
+        if isinstance(index_col, str):
+            index_col = [index_col]
+        sdf_columns = set(sdf.columns)
+        for col in index_col:
+            if col not in sdf_columns:
+                raise KeyError(col)
+        index_map = [(col, col) for col in index_col]
+    else:
+        index_map = None
+
+    return index_map
 
 
 _get_dummies_default_accept_types = (
