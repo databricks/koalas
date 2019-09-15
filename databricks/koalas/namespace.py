dc2d31d9f14eb8f3f117de004c1c968e4c6e7b1e
itholic <haejoon309@naver.com>
2019-09-15 16:49:22 +0900

    Remove type hint for consitency other parameter

--- databricks/koalas/namespace.py
+++ databricks/koalas/namespace.py
@@ -124,15 +124,8 @@ def range(start: int,
     return DataFrame(sdf)
 
 
-def read_csv(path,
-             sep=',',
-             header='infer',
-             names=None,
-             index_col: Optional[Union[str, List[str]]] = None,
-             usecols=None,
-             mangle_dupe_cols=True,
-             parse_dates=False,
-             comment=None):
+def read_csv(path, sep=',', header='infer', names=None, index_col=None,
+             usecols=None, mangle_dupe_cols=True, parse_dates=False, comment=None):
     """Read CSV (comma-separated) file into DataFrame.
 
     Parameters
