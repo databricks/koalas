8d3cd321a976f12ff3c5df7a14375dd335a599d0
itholic <haejoon309@naver.com>
2019-09-17 22:42:10 +0900

    Implement 'pop' for DataFrame

--- databricks/koalas/frame.py
+++ databricks/koalas/frame.py
@@ -1700,6 +1700,47 @@ defaultdict(<class 'list'>, {'col..., 'col...})]
 
         return DataFrame(internal)
 
+    def pop(self, item):
+        """
+        Return item and drop from frame. Raise KeyError if not found.
+        Parameters
+        ----------
+        item : str
+            Label of column to be popped.
+        Returns
+        -------
+        Series
+        Examples
+        --------
+        >>> df = ks.DataFrame([('falcon', 'bird', 389.0),
+        ...                    ('parrot', 'bird', 24.0),
+        ...                    ('lion', 'mammal', 80.5),
+        ...                    ('monkey','mammal', np.nan)],
+        ...                   columns=('name', 'class', 'max_speed'))
+        >>> df
+             name   class  max_speed
+        0  falcon    bird      389.0
+        1  parrot    bird       24.0
+        2    lion  mammal       80.5
+        3  monkey  mammal        NaN
+        >>> df.pop('class')
+        0      bird
+        1      bird
+        2    mammal
+        3    mammal
+        Name: class, dtype: object
+        >>> df
+             name  max_speed
+        0  falcon      389.0
+        1  parrot       24.0
+        2    lion       80.5
+        3  monkey        NaN
+        """
+        result = self[item]
+        del self._internal[item]
+
+        return result
+
     @property
     def index(self):
         """The index (row labels) Column of the DataFrame.
