e23f9ee086a7bc8adb1f0b62decbd7f4143dfe3f
itholic <haejoon309@naver.com>
2019-09-20 08:58:03 +0900

    Remove pandas part from doctest

--- databricks/koalas/frame.py
+++ databricks/koalas/frame.py
@@ -4344,17 +4344,16 @@ defaultdict(<class 'list'>, {'col..., 'col...})]
         0  1  7
         1  2  8
 
-        >>> pdf = pd.DataFrame({'x': [1, 2], 'y': [3, 4], 'z': [5, 6], 'w': [7, 8]},
-        ...                    columns=['x', 'y', 'z', 'w'])
+        >>> df = ks.DataFrame({'x': [1, 2], 'y': [3, 4], 'z': [5, 6], 'w': [7, 8]},
+        ...                   columns=['x', 'y', 'z', 'w'])
         >>> columns = [('a', 'x'), ('a', 'y'), ('b', 'z'), ('b', 'w')]
-        >>> pdf.columns = pd.MultiIndex.from_tuples(columns)
-        >>> kdf = ks.DataFrame(pdf)
-        >>> kdf  # doctest: +NORMALIZE_WHITESPACE
+        >>> df.columns = pd.MultiIndex.from_tuples(columns)
+        >>> df  # doctest: +NORMALIZE_WHITESPACE
            a     b
            x  y  z  w
         0  1  3  5  7
         1  2  4  6  8
-        >>> kdf.drop('a')  # doctest: +NORMALIZE_WHITESPACE
+        >>> df.drop('a')  # doctest: +NORMALIZE_WHITESPACE
            b
            z  w
         0  5  7
