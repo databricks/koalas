cf4ded8786c0832a4b85b51ce7370cb56e828aa2
HaeJoon Lee <44108233+itholic@users.noreply.github.com>
2019-09-28 10:09:29 +0900

    Add DataFrame.plot.kde() (an alias of 'density') (#784)
    This PR implements kde in DataFrame. It reuses Series' implementation. at #767  
    
    like Series's kde plot, Since DataFrame's also uses MLlib's KernelDensity API to calculate KDE , so slightly different from pandas but seems good enough either:
    
    <img width="884" alt="스크린샷 2019-09-17 오후 2 24 34" src="https://user-images.githubusercontent.com/44108233/65013645-e3933100-d956-11e9-9166-be6d534046bd.png">
    <img width="882" alt="스크린샷 2019-09-17 오후 2 25 18" src="https://user-images.githubusercontent.com/44108233/65013684-ff96d280-d956-11e9-8714-6c860c4e2c13.png">
    
    And also kde is an alias of 'density', you can get exactly same result when you use 'density' rather than 'kde' like below:
    
    <img width="886" alt="스크린샷 2019-09-17 오후 2 27 30" src="https://user-images.githubusercontent.com/44108233/65013769-66b48700-d957-11e9-84f0-5b5989ed2d49.png">
    <img width="882" alt="스크린샷 2019-09-17 오후 2 27 09" src="https://user-images.githubusercontent.com/44108233/65013770-67e5b400-d957-11e9-875c-0fd73b32fa5f.png">
    
    **Multiple columns examples:**
    
    <img width="819" alt="스크린샷 2019-09-17 오후 2 55 35" src="https://user-images.githubusercontent.com/44108233/65015007-45ee3080-d95b-11e9-9c31-a4b85631e404.png">
    
    and for each row as Series.plot.kde looks same like below:
    
    <img width="739" alt="스크린샷 2019-09-17 오후 2 57 57" src="https://user-images.githubusercontent.com/44108233/65015172-b9903d80-d95b-11e9-8245-8b47190c38b6.png">

--- databricks/koalas/plot.py
+++ databricks/koalas/plot.py
@@ -1246,8 +1246,30 @@ class KoalasFramePlotMethods(PandasObject):
         """
         return self(kind='line', x=x, y=y, **kwargs)
 
-    def kde(self, bw_method=None, ind=None, **kwds):
-        return _unsupported_function(class_name='pd.DataFrame', method_name='kde')()
+    def kde(self, bw_method=None, ind=None, **kwargs):
+        """
+        Generate Kernel Density Estimate plot using Gaussian kernels.
+
+        Parameters
+        ----------
+        bw_method : scalar
+            The method used to calculate the estimator bandwidth.
+            See KernelDensity in PySpark for more information.
+        ind : NumPy array or integer, optional
+            Evaluation points for the estimated PDF. If None (default),
+            1000 equally spaced points are used. If `ind` is a NumPy array, the
+            KDE is evaluated at the points passed. If `ind` is an integer,
+            `ind` number of equally spaced points are used.
+        **kwargs : optional
+            Keyword arguments to pass on to :meth:`Koalas.DataFrame.plot`.
+
+        Returns
+        -------
+        matplotlib.axes.Axes or numpy.ndarray of them
+        """
+        return self(kind="kde", bw_method=bw_method, ind=ind, **kwargs)
+
+    density = kde
 
     def pie(self, y=None, **kwds):
         """
@@ -1361,9 +1383,6 @@ class KoalasFramePlotMethods(PandasObject):
 
     def hexbin(self, **kwds):
         return _unsupported_function(class_name='pd.DataFrame', method_name='hexbin')()
-
-    def density(self, **kwds):
-        return _unsupported_function(class_name='pd.DataFrame', method_name='density')()
 
     def box(self, **kwds):
         return _unsupported_function(class_name='pd.DataFrame', method_name='box')()
