508993dc7ff01a5c8371d0c6a150dd90413d0cf2
itholic <haejoon309@naver.com>
2019-09-30 13:33:28 +0900

    For PEP8

--- databricks/koalas/series.py
+++ databricks/koalas/series.py
@@ -1505,9 +1505,9 @@ class Series(_Frame, IndexOpsMixin, Generic[T]):
         Also support for MultiIndex
 
         >>> midx = pd.MultiIndex([['lama', 'cow', 'falcon'],
-        ...                              ['speed', 'weight', 'length']],
+        ...                       ['speed', 'weight', 'length']],
         ...                      [[0, 0, 0, 1, 1, 1, 2, 2, 2],
-        ...                             [0, 1, 2, 0, 1, 2, 0, 1, 2]])
+        ...                       [0, 1, 2, 0, 1, 2, 0, 1, 2]])
         >>> s = ks.Series([45, 200, 1.2, 30, 250, 1.5, 320, 1, 0.3],
         ...               index=midx)
         >>> s
