3085759edd281493ff0a02b6d2d6e5ac8b1c0648
itholic <haejoon309@naver.com>
2019-10-19 16:34:37 +0900

    Add Example

--- databricks/koalas/frame.py
+++ databricks/koalas/frame.py
@@ -7413,6 +7413,20 @@ defaultdict(<class 'list'>, {'col..., 'col...})]
         -------
         Index
             Columns of the DataFrame.
+
+        Examples
+        --------
+        >>> df = ks.DataFrame([[1, 2], [4, 5], [7, 8]],
+        ...                   index=['cobra', 'viper', 'sidewinder'],
+        ...                   columns=['max_speed', 'shield'])
+        >>> df
+                    max_speed  shield
+        cobra               1       2
+        viper               4       5
+        sidewinder          7       8
+
+        >>> df.keys()
+        Index(['max_speed', 'shield'], dtype='object')
         """
         return self.columns
 
