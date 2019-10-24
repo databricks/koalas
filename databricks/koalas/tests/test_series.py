caac3674ed818a26b14d2a90f91eb2af14b75b6a
HaeJoon Lee <44108233+itholic@users.noreply.github.com>
2019-10-24 13:35:27 +0900

    Implement Series.truncate (#928)
    Like pandas Series.truncate (https://pandas.pydata.org/pandas-docs/version/0.21/generated/pandas.Series.truncate.html#pandas.Series.truncate)
    
    implemented function `truncate` for Series.
    
    ```python
    >>> s = ks.Series([10, 20, 30, 40, 50, 60, 70],
    ...               index=[1, 2, 3, 4, 5, 6, 7])
    >>> s
    1    10
    2    20
    3    30
    4    40
    5    50
    6    60
    7    70
    Name: 0, dtype: int64
    
    >>> s.truncate(2, 5)
    2    20
    3    30
    4    40
    5    50
    Name: 0, dtype: int64
    
    >>> s = ks.Series([10, 20, 30, 40, 50, 60, 70],
    ...               index=['a', 'b', 'c', 'd', 'e', 'f', 'g'])
    >>> s
    a    10
    b    20
    c    30
    d    40
    e    50
    f    60
    g    70
    Name: 0, dtype: int64
    
    >>> s.truncate('b', 'e')
    b    20
    c    30
    d    40
    e    50
    Name: 0, dtype: int64
    ```

--- databricks/koalas/tests/test_series.py
+++ databricks/koalas/tests/test_series.py
@@ -729,6 +729,30 @@ class SeriesTest(ReusedSQLTestCase, SQLTestUtils):
         self.assert_eq(pser.drop_duplicates().sort_values(),
                        kser.drop_duplicates().sort_values())
 
+    def test_truncate(self):
+        pser1 = pd.Series([10, 20, 30, 40, 50, 60, 70], index=[1, 2, 3, 4, 5, 6, 7])
+        kser1 = ks.Series(pser1)
+        pser2 = pd.Series([10, 20, 30, 40, 50, 60, 70], index=[7, 6, 5, 4, 3, 2, 1])
+        kser2 = ks.Series(pser2)
+
+        self.assert_eq(kser1.truncate(), pser1.truncate())
+        self.assert_eq(kser1.truncate(before=2), pser1.truncate(before=2))
+        self.assert_eq(kser1.truncate(after=5), pser1.truncate(after=5))
+        self.assert_eq(kser1.truncate(copy=False), pser1.truncate(copy=False))
+        self.assert_eq(kser1.truncate(2, 5, copy=False), pser1.truncate(2, 5, copy=False))
+        self.assert_eq(kser2.truncate(4, 6), pser2.truncate(4, 6))
+        self.assert_eq(kser2.truncate(4, 6, copy=False), pser2.truncate(4, 6, copy=False))
+
+        kser = ks.Series([10, 20, 30, 40, 50, 60, 70], index=[1, 2, 3, 4, 3, 2, 1])
+        msg = "truncate requires a sorted index"
+        with self.assertRaisesRegex(ValueError, msg):
+            kser.truncate()
+
+        kser = ks.Series([10, 20, 30, 40, 50, 60, 70], index=[1, 2, 3, 4, 5, 6, 7])
+        msg = "Truncate: 2 must be after 5"
+        with self.assertRaisesRegex(ValueError, msg):
+            kser.truncate(5, 2)
+
     def test_getitem(self):
         pser = pd.Series([10, 20, 15, 30, 45], ['A', 'A', 'B', 'C', 'D'])
         kser = ks.Series(pser)
