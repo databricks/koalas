651ee907be5b2f4a6453c2f77086e203a7702b6d
Kaiqi <kaiqi@kth.se>
2019-10-29 18:12:23 +0800

    add test for get_level_number

--- databricks/koalas/tests/test_indexes.py
+++ databricks/koalas/tests/test_indexes.py
@@ -299,3 +299,43 @@ class IndexesTest(ReusedSQLTestCase, TestUtils):
 
         for level in [0, 1, 'level_1', 'level_2']:
             self.assert_eq(kdf.index.get_level_values(level), pdf.index.get_level_values(level))
+
+    def test_index_get_level_number(self):
+        # name of two levels are the same, which is None
+        kdf = ks.DataFrame({'a': [1, 2, 3]}, index=[list('aac'), list('ddf')])
+        with self.assertRaisesRegex(ValueError,
+                                    "The name None occurs multiple times, use a level number"):
+            kdf.index._get_level_number(None)
+
+        mi = pd.MultiIndex.from_arrays((list('abc'), list('def')))
+        mi.names = ['level_1', 'level_2']
+        kdf = ks.DataFrame({'a': [1, 2, 3]}, index=mi)
+
+        # level is not int and not in the level name list
+        with self.assertRaisesRegexp(KeyError,
+                                     "Level lv_3 not found"):
+            kdf.index._get_level_number('lv_3')
+
+        # level is int, but an invalid negative number
+        with self.assertRaisesRegexp(IndexError,
+                                     "Too many levels: Index has only"):
+            kdf.index._get_level_number(-3)
+
+        # level is int, but an invalid positive number
+        with self.assertRaisesRegexp(IndexError,
+                                     "Too many levels: Index has only"):
+            kdf.index._get_level_number(3)
+
+        # Correct and valid inputs in numbers
+        level_number = [-2, -1, 0, 1]
+        outputs = [0, 1, 0, 1]
+
+        for lv, output in zip(level_number, outputs):
+            self.assertEqual(output, kdf.index._get_level_number(lv))
+
+        # Valid inputs as level names
+        level_names = ['level_1', 'level_2']
+        outputs = [0, 1]
+
+        for lv, output in zip(level_names, outputs):
+            self.assertEqual(output, kdf.index._get_level_number(lv))
