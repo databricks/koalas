345f91e5836bdd4614bd1e2d87f8afbbfb2173c3
FWANI <seungfwani@gmail.com>
2019-10-31 00:46:35 +0900

    Add test of drop_duplicates with inplace parameter

--- databricks/koalas/tests/test_dataframe.py
+++ databricks/koalas/tests/test_dataframe.py
@@ -1699,6 +1699,7 @@ class DataFrameTest(ReusedSQLTestCase, SQLTestUtils):
         pdf = pd.DataFrame({'a': [1, 2, 2, 2, 3], 'b': ['a', 'a', 'a', 'c', 'd']})
         kdf = ks.from_pandas(pdf)
 
+        # inplace is False
         self.assert_eq(pdf.drop_duplicates().sort_index(),
                        kdf.drop_duplicates().sort_index())
         self.assert_eq(pdf.drop_duplicates('a').sort_index(),
@@ -1706,7 +1707,7 @@ class DataFrameTest(ReusedSQLTestCase, SQLTestUtils):
         self.assert_eq(pdf.drop_duplicates(['a', 'b']).sort_index(),
                        kdf.drop_duplicates(['a', 'b']).sort_index())
 
-        # multi-index columns
+        # multi-index columns, inplace is False
         columns = pd.MultiIndex.from_tuples([('x', 'a'), ('y', 'b')])
         pdf.columns = columns
         kdf.columns = columns
@@ -1717,6 +1718,29 @@ class DataFrameTest(ReusedSQLTestCase, SQLTestUtils):
                        kdf.drop_duplicates(('x', 'a')).sort_index())
         self.assert_eq(pdf.drop_duplicates([('x', 'a'), ('y', 'b')]).sort_index(),
                        kdf.drop_duplicates([('x', 'a'), ('y', 'b')]).sort_index())
+
+        # inplace is True
+        subset_list = [None, 'a', ['a', 'b']]
+        for subset in subset_list:
+            pdf = pd.DataFrame({'a': [1, 2, 2, 2, 3], 'b': ['a', 'a', 'a', 'c', 'd']})
+            kdf = ks.from_pandas(pdf)
+            pdf.drop_duplicates(subset=subset, inplace=True)
+            kdf.drop_duplicates(subset=subset, inplace=True)
+            self.assert_eq(pdf.sort_index(),
+                           kdf.sort_index())
+
+        # multi-index columns, inplace is True
+        subset_list = [None, ('x', 'a'), [('x', 'a'), ('y', 'b')]]
+        for subset in subset_list:
+            pdf = pd.DataFrame({'a': [1, 2, 2, 2, 3], 'b': ['a', 'a', 'a', 'c', 'd']})
+            kdf = ks.from_pandas(pdf)
+            columns = pd.MultiIndex.from_tuples([('x', 'a'), ('y', 'b')])
+            pdf.columns = columns
+            kdf.columns = columns
+            pdf.drop_duplicates(subset=subset, inplace=True)
+            kdf.drop_duplicates(subset=subset, inplace=True)
+            self.assert_eq(pdf.sort_index(),
+                           kdf.sort_index())
 
     def test_reindex(self):
         index = ['A', 'B', 'C', 'D', 'E']
