201f4cab1192760607f7f7d4a26339ffe5e2f5bf
Kaiqi <kaiqi@kth.se>
2019-11-04 22:44:34 +0100

    Fix named aggregation for MultiIndex

--- databricks/koalas/groupby.py
+++ databricks/koalas/groupby.py
@@ -211,6 +211,10 @@ class GroupBy(object):
             kdf = kdf.reset_index(drop=self._should_drop_index)
 
         if relabeling:
+
+            # For MultiIndex, we need to flatten the tuple, e.g. (('y', 'A'), 'max') needs to be
+            # flattened to ('y', 'A', 'max'), it won't do anything on normal Index.
+            order = [(*levs, method) for levs, method in order]
             kdf = kdf[order]
             kdf.columns = columns
         return kdf
--- databricks/koalas/tests/test_groupby.py
+++ databricks/koalas/tests/test_groupby.py
@@ -435,6 +435,54 @@ class GroupByTest(ReusedSQLTestCase, TestUtils):
             describe_pdf = pdf.groupby("a").describe().sort_index()
             self.assertRaises(NotImplementedError, lambda: kdf.groupby("a").describe().sort_index())
 
+    @unittest.skipIf(pd.__version__ < "0.25.0", "not supported before pandas 0.25.0")
+    def test_aggregate_relabel_multiindex(self):
+        pdf = pd.DataFrame({
+            "group": ['a', 'a', 'b', 'b'],
+            "A": [0, 1, 2, 3],
+            "B": [5, 6, 7, 8]
+        })
+        pdf.columns = pd.MultiIndex.from_tuples([('x', 'group'), ('y', 'A'), ('y', 'B')])
+        kdf = ks.from_pandas(pdf)
+
+        agg_pdf = (
+            pdf.groupby(('x', 'group'))
+               .agg(a_max=(('y', 'A'), "max"))
+               .sort_index()
+        )
+        agg_kdf = (
+            kdf.groupby(('x', 'group'))
+               .agg(a_max=(('y', 'A'), "max"))
+               .sort_index()
+        )
+        self.assert_eq(agg_pdf, agg_kdf)
+
+        # same column, different methods
+        agg_pdf = (
+            pdf.groupby(('x', 'group'))
+               .agg(a_max=(('y', 'A'), "max"), a_min=(('y', 'A'), "min"))
+               .sort_index()
+        )
+        agg_kdf = (
+            kdf.groupby(('x', 'group'))
+               .agg(a_max=(('y', 'A'), "max"), a_min=(('y', 'A'), "min"))
+               .sort_index()
+        )
+        self.assert_eq(agg_pdf, agg_kdf)
+
+        # different column, different methods
+        agg_pdf = (
+            pdf.groupby(('x', 'group'))
+               .agg(a_max=(('y', 'B'), "max"), a_min=(('y', 'A'), "min"))
+               .sort_index()
+        )
+        agg_kdf = (
+            kdf.groupby(('x', 'group'))
+               .agg(a_max=(('y', 'B'), "max"), a_min=(('y', 'A'), "min"))
+               .sort_index()
+        )
+        self.assert_eq(agg_pdf, agg_kdf)
+
     def test_all_any(self):
         pdf = pd.DataFrame(
             {
