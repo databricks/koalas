37902f96d616988ea2cde4b711069bd3b8a97420
itholic <haejoon309@naver.com>
2019-11-06 14:22:25 +0900

    fix failure

--- databricks/koalas/frame.py
+++ databricks/koalas/frame.py
@@ -7540,10 +7540,10 @@ defaultdict(<class 'list'>, {'col..., 'col...})]
         # +---------+---------+
 
         cols_dict = OrderedDict()
-        for column in self.columns:
+        for column in self._internal.data_columns:
             cols_dict[column] = list()
             for i in range(len(quantiles)):
-                cols_dict[column].append(self._internal.scol_for(column).getItem(i).alias(column))
+                cols_dict[column].append(F.col(column).getItem(i).alias(column))
 
         internal_index_column = SPARK_INDEX_NAME_FORMAT(0)
         cols = []
