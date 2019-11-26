06b9832d806f280a37c5194c7961e715fe672077
Yao Gu <guyao@guyao.me>
2019-11-25 20:40:01 -0800

    update document

--- databricks/koalas/frame.py
+++ databricks/koalas/frame.py
@@ -1101,13 +1101,48 @@ class DataFrame(_Frame, Generic[T]):
         return list((col_name, self[col_name]) for col_name in cols)
 
     def iterrows(self):
-        # TODO: document
+        """
+        Iterate over DataFrame rows as (index, Series) pairs.
+        Yields
+        ------
+        index : label or tuple of label
+            The index of the row. A tuple for a `MultiIndex`.
+        data : pandas.Series
+            The data of the row as a Series.
+        it : generator
+            A generator that iterates over the rows of the frame.
+
+        Notes
+        -----
+        1. Because ``iterrows`` returns a Series for each row,
+           it does **not** preserve dtypes across the rows (dtypes are
+           preserved across columns for DataFrames). For example,
+           >>> df = pd.DataFrame([[1, 1.5]], columns=['int', 'float'])
+           >>> row = next(df.iterrows())[1]
+           >>> row
+           int      1.0
+           float    1.5
+           Name: 0, dtype: float64
+           >>> print(row['int'].dtype)
+           float64
+           >>> print(df['int'].dtype)
+           int64
+           To preserve dtypes while iterating over the rows, it is better
+           to use :meth:`itertuples` which returns namedtuples of the values
+           and which is generally faster than ``iterrows``.
+        2. You should **never modify** something you are iterating over.
+           This is not guaranteed to work in all cases. Depending on the
+           data types, the iterator returns a copy and not a view, and writing
+           to it will have no effect.
+        """
+
         columns = self.columns
         internal_index_columns = self._internal.index_columns
         internal_data_columns = self._internal.data_columns
 
         def extract_kv_from_spark_row(row):
-            k = tuple(row[c] for c in internal_index_columns)
+            k = row[internal_index_columns[0]] if len(internal_index_columns) == 1 else tuple(
+                row[c] for c in internal_index_columns)
             v = [row[c] for c in internal_data_columns]
             return k, v
 
