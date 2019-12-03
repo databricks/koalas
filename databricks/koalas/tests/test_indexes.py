c6b4ced3a493ea1bd91669d3f4cc9a7bed0a3102
fengyurun <fengyurun1@jd.com>
2019-12-03 18:04:54 +0800

    remove name from multiindex test

--- databricks/koalas/tests/test_indexes.py
+++ databricks/koalas/tests/test_indexes.py
@@ -151,12 +151,9 @@ class IndexesTest(ReusedSQLTestCase, TestUtils):
             kidx.name = 'renamed'
 
     def test_multi_index_levshape(self):
-        arrays = [[1, 1, 2, 2], ['red', 'blue', 'red', 'blue']]
-        idx = pd.MultiIndex.from_arrays(arrays, names=('number', 'color'))
-        pdf = pd.DataFrame(np.random.randn(4, 5), idx)
-        kdf = ks.from_pandas(pdf)
-
-        self.assertEqual(kdf.index.levshape, pdf.index.levshape)
+        pidx = pd.MultiIndex.from_tuples([('a', 'x', 1), ('b', 'y', 2)])
+        kidx = ks.MultiIndex.from_tuples([('a', 'x', 1), ('b', 'y', 2)])
+        self.assertEqual(pidx.levshape, kidx.levshape)
 
     def test_index_unique(self):
         kidx = self.kdf.index
