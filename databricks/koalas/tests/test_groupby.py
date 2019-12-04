bbf956b15ec3f8956b37058a9ebc3b61e8fa2cac
itholic <haejoon309@naver.com>
2019-12-04 12:44:49 +0900

    add test for coverage

--- databricks/koalas/tests/test_groupby.py
+++ databricks/koalas/tests/test_groupby.py
@@ -71,10 +71,12 @@ class GroupByTest(ReusedSQLTestCase, TestUtils):
         # we can't use column name/names as a parameter `by` for `SeriesGroupBy`.
         self.assertRaises(KeyError, lambda: kdf.a.groupby(by='a'))
         self.assertRaises(KeyError, lambda: kdf.a.groupby(by=['a', 'b']))
+        self.assertRaises(KeyError, lambda: kdf.a.groupby(by=('a', 'b')))
 
         # we can't use DataFrame as a parameter `by` for `DataFrameGroupBy`/`SeriesGroupBy`.
         self.assertRaises(ValueError, lambda: kdf.groupby(kdf))
         self.assertRaises(ValueError, lambda: kdf.a.groupby(kdf))
+        self.assertRaises(ValueError, lambda: kdf.a.groupby((kdf,)))
 
     def test_groupby_multiindex_columns(self):
         pdf = pd.DataFrame({('x', 'a'): [1, 2, 6, 4, 4, 6, 4, 3, 7],
