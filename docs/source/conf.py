1f9904a41a756ae49cd98c45481a1f505394a4cb
harupy <hkawamura0130@gmail.com>
2020-01-10 22:12:26 +0900

    Fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -125,7 +125,6 @@ html_domain_indices = False
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#
 html_favicon = 'favicon.ico'
 
 
