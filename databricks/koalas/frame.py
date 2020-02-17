fd8c5ca1e1f1badb51952c5b44807c4f3cbef86f
itholic <haejoon309@naver.com>
2020-02-17 15:44:19 +0900

    fix docstring for DataFrame

--- databricks/koalas/frame.py
+++ databricks/koalas/frame.py
@@ -411,7 +411,7 @@ class DataFrame(_Frame, Generic[T]):
     def axes(self):
         """
         Return a list representing the axes of the DataFrame.
-        
+
         It has the row axis labels and column axis labels as the only members.
         They are returned in that order.
 
@@ -421,10 +421,6 @@ class DataFrame(_Frame, Generic[T]):
         >>> df = ks.DataFrame({'col1': [1, 2], 'col2': [3, 4]})
         >>> df.axes
         [RangeIndex(start=0, stop=2, step=1), Index(['col1', 'col2'], dtype='object')]
-
-        MultiIndex (for Series also)
-
-        MultiIndex columns
         """
         return [self.index, self.columns]
 
