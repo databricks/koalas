78942cc5fc8175b1bc5f266c876668d8c9fe1113
Hyukjin Kwon <gurwls223@apache.org>
2020-03-13 03:30:28 +0900

    Add the support for operations between different DataFrames in groupby() (#1321)
    This PR proposes to support the operations between series from different DataFrames in `groupby()`.
    
    ```python
    import databricks.koalas as ks
    ks.set_option('compute.ops_on_diff_frames', True)
    kdf = ks.DataFrame({"a": [1, 2, 6, 4, 4, 6, 4, 7], "b": [4, 2, 7, 3, 3, 1, 1, 2]})
    kdf.groupby(ks.Series([4, 2, 7, 3, 1, 1, 1, 2, 2])).sum()
    ```
    
    The approach is similar with other implementations of `compute.ops_on_diff_frames`.
    
    Before:
    
    ```
    Traceback (most recent call last):
      File "<stdin>", line 1, in <module>
      File "/.../koalas/databricks/koalas/generic.py", line 1437, in groupby
        col_by = [_resolve_col(df, col_or_s) for col_or_s in by]
      File "/.../koalas/databricks/koalas/generic.py", line 1437, in <listcomp>
        col_by = [_resolve_col(df, col_or_s) for col_or_s in by]
      File "/.../koalas/databricks/koalas/generic.py", line 1835, in _resolve_col
        "Cannot combine the series because it comes from a different dataframe. "
    ValueError: Cannot combine the series because it comes from a different dataframe. In order to allow this operation, enable 'compute.ops_on_diff_frames' option.
    ```
    
    After:
    
    ```
        a  b
    0
    7   6  7
    1  14  5
    3   4  3
    2   9  4
    4   1  4
    ```

---
+++ databricks/koalas/tests/test_ops_on_diff_frames_groupby.py
@@ -0,0 +1,169 @@
+#
+# Copyright (C) 2019 Databricks, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import pandas as pd
+
+from databricks import koalas as ks
+from databricks.koalas.config import set_option, reset_option
+from databricks.koalas.testing.utils import ReusedSQLTestCase, SQLTestUtils
+
+
+class OpsOnDiffFramesGroupByTest(ReusedSQLTestCase, SQLTestUtils):
+    @classmethod
+    def setUpClass(cls):
+        super(OpsOnDiffFramesGroupByTest, cls).setUpClass()
+        set_option("compute.ops_on_diff_frames", True)
+
+    @classmethod
+    def tearDownClass(cls):
+        reset_option("compute.ops_on_diff_frames")
+        super(OpsOnDiffFramesGroupByTest, cls).tearDownClass()
+
+    def test_groupby_different_lengths(self):
+        pdfs1 = [
+            pd.DataFrame({"c": [4, 2, 7, 3, None, 1, 1, 1, 2], "d": list("abcdefght")}),
+            pd.DataFrame({"c": [4, 2, 7, None, 1, 1, 2], "d": list("abcdefg")}),
+            pd.DataFrame({"c": [4, 2, 7, 3, None, 1, 1, 1, 2, 2], "d": list("abcdefghti")}),
+        ]
+        pdfs2 = [
+            pd.DataFrame({"a": [1, 2, 6, 4, 4, 6, 4, 3, 7], "b": [4, 2, 7, 3, 3, 1, 1, 1, 2]}),
+            pd.DataFrame({"a": [1, 2, 6, 4, 4, 6, 4, 7], "b": [4, 2, 7, 3, 3, 1, 1, 2]}),
+            pd.DataFrame({"a": [1, 2, 6, 4, 4, 6, 4, 3, 7], "b": [4, 2, 7, 3, 3, 1, 1, 1, 2]}),
+        ]
+
+        for pdf1, pdf2 in zip(pdfs1, pdfs2):
+            kdf1 = ks.from_pandas(pdf1)
+            kdf2 = ks.from_pandas(pdf2)
+
+            for as_index in [True, False]:
+                if as_index:
+                    sort = lambda df: df.sort_index()
+                else:
+                    sort = lambda df: df.sort_values("c").reset_index(drop=True)
+                self.assert_eq(
+                    sort(kdf1.groupby(kdf2.a, as_index=as_index).sum()),
+                    sort(pdf1.groupby(pdf2.a, as_index=as_index).sum()),
+                )
+
+                self.assert_eq(
+                    sort(kdf1.groupby(kdf2.a, as_index=as_index).c.sum()),
+                    sort(pdf1.groupby(pdf2.a, as_index=as_index).c.sum()),
+                )
+                self.assert_eq(
+                    sort(kdf1.groupby(kdf2.a, as_index=as_index)["c"].sum()),
+                    sort(pdf1.groupby(pdf2.a, as_index=as_index)["c"].sum()),
+                )
+
+    def test_groupby_multiindex_columns(self):
+        pdf1 = pd.DataFrame(
+            {("y", "c"): [4, 2, 7, 3, None, 1, 1, 1, 2], ("z", "d"): list("abcdefght"),}
+        )
+        pdf2 = pd.DataFrame(
+            {("x", "a"): [1, 2, 6, 4, 4, 6, 4, 3, 7], ("x", "b"): [4, 2, 7, 3, 3, 1, 1, 1, 2],}
+        )
+        kdf1 = ks.from_pandas(pdf1)
+        kdf2 = ks.from_pandas(pdf2)
+
+        self.assert_eq(
+            kdf1.groupby(kdf2[("x", "a")]).sum().sort_index(),
+            pdf1.groupby(pdf2[("x", "a")]).sum().sort_index(),
+        )
+
+        self.assert_eq(
+            kdf1.groupby(kdf2[("x", "a")], as_index=False)
+            .sum()
+            .sort_values(("y", "c"))
+            .reset_index(drop=True),
+            pdf1.groupby(pdf2[("x", "a")], as_index=False)
+            .sum()
+            .sort_values(("y", "c"))
+            .reset_index(drop=True),
+        )
+        self.assert_eq(
+            kdf1.groupby(kdf2[("x", "a")])[[("y", "c")]].sum().sort_index(),
+            pdf1.groupby(pdf2[("x", "a")])[[("y", "c")]].sum().sort_index(),
+        )
+
+    def test_aggregate(self):
+        pdf1 = pd.DataFrame({"C": [0.362, 0.227, 1.267, -0.562], "B": [1, 2, 3, 4]})
+        pdf2 = pd.DataFrame({"A": [1, 1, 2, 2]})
+        kdf1 = ks.from_pandas(pdf1)
+        kdf2 = ks.from_pandas(pdf2)
+
+        for as_index in [True, False]:
+            stats_kdf = kdf1.groupby(kdf2.A, as_index=as_index).agg({"B": "min", "C": "sum"})
+            stats_pdf = pdf1.groupby(pdf2.A, as_index=as_index).agg({"B": "min", "C": "sum"})
+            self.assert_eq(
+                stats_kdf.sort_values(by=["B", "C"]).reset_index(drop=True),
+                stats_pdf.sort_values(by=["B", "C"]).reset_index(drop=True),
+            )
+
+            stats_kdf = kdf1.groupby(kdf2.A, as_index=as_index).agg(
+                {"B": ["min", "max"], "C": "sum"}
+            )
+            stats_pdf = pdf1.groupby(pdf2.A, as_index=as_index).agg(
+                {"B": ["min", "max"], "C": "sum"}
+            )
+            self.assert_eq(
+                stats_kdf.sort_values(by=[("B", "min"), ("B", "max"), ("C", "sum")]).reset_index(
+                    drop=True
+                ),
+                stats_pdf.sort_values(by=[("B", "min"), ("B", "max"), ("C", "sum")]).reset_index(
+                    drop=True
+                ),
+            )
+
+        # multi-index columns
+        columns = pd.MultiIndex.from_tuples([("Y", "C"), ("X", "B")])
+        pdf1.columns = columns
+        kdf1.columns = columns
+
+        columns = pd.MultiIndex.from_tuples([("X", "A")])
+        pdf2.columns = columns
+        kdf2.columns = columns
+
+        for as_index in [True, False]:
+            stats_kdf = kdf1.groupby(kdf2[("X", "A")], as_index=as_index).agg(
+                {("X", "B"): "min", ("Y", "C"): "sum"}
+            )
+            stats_pdf = pdf1.groupby(pdf2[("X", "A")], as_index=as_index).agg(
+                {("X", "B"): "min", ("Y", "C"): "sum"}
+            )
+            self.assert_eq(
+                stats_kdf.sort_values(by=[("X", "B"), ("Y", "C")]).reset_index(drop=True),
+                stats_pdf.sort_values(by=[("X", "B"), ("Y", "C")]).reset_index(drop=True),
+            )
+
+        stats_kdf = kdf1.groupby(kdf2[("X", "A")]).agg(
+            {("X", "B"): ["min", "max"], ("Y", "C"): "sum"}
+        )
+        stats_pdf = pdf1.groupby(pdf2[("X", "A")]).agg(
+            {("X", "B"): ["min", "max"], ("Y", "C"): "sum"}
+        )
+        self.assert_eq(
+            stats_kdf.sort_values(
+                by=[("X", "B", "min"), ("X", "B", "max"), ("Y", "C", "sum")]
+            ).reset_index(drop=True),
+            stats_pdf.sort_values(
+                by=[("X", "B", "min"), ("X", "B", "max"), ("Y", "C", "sum")]
+            ).reset_index(drop=True),
+        )
+
+    def test_duplicated_labels(self):
+        kdf1 = ks.DataFrame({"A": [3, 2, 1]})
+        kdf2 = ks.DataFrame({"A": [1, 2, 3]})
+        self.assertRaisesRegex(
+            NotImplementedError, "Duplicated labels with group", lambda: kdf1.groupby(kdf2.A),
+        )
