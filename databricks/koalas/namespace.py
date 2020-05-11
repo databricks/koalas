b14d3fae8c8ecb68376eef2a7eaf8f6d41ed7f6b
Haejoon Lee <44108233+itholic@users.noreply.github.com>
2020-05-11 15:51:02 +0900

    Fix get dummies when uses the prefix parameter whose type is dict (#1478)
    Resolves #1469 
    
    ```python
    >>> df = ks.DataFrame({'A': ['a', 'b', 'a'], 'B': ['b', 'a', 'c'], 'C': [1, 2, 3]}, columns=['A', 'B', 'C'])
    
    >>> ks.get_dummies(df, prefix={'B': 'ohe_B', 'C': 'ohe_C'}, columns=['B', 'C'])
       A  ohe_B_a  ohe_B_b  ohe_B_c  ohe_C_1  ohe_C_2  ohe_C_3
    0  a        0        1        0        1        0        0
    1  b        1        0        0        0        1        0
    2  a        0        0        1        0        0        1
    
    >>> pd.get_dummies(df.to_pandas(), prefix={'B': 'ohe_B', 'C': 'ohe_C'}, columns=['B', 'C'])
       A  ohe_B_a  ohe_B_b  ohe_B_c  ohe_C_1  ohe_C_2  ohe_C_3
    0  a        0        1        0        1        0        0
    1  b        1        0        0        0        1        0
    2  a        0        0        1        0        0        1
    ```

--- databricks/koalas/namespace.py
+++ databricks/koalas/namespace.py
@@ -1576,6 +1576,8 @@ def get_dummies(
             "Length of 'prefix' ({}) did not match the length of "
             "the columns being encoded ({}).".format(len(prefix), len(column_labels))
         )
+    elif isinstance(prefix, dict):
+        prefix = [prefix[column_label[0]] for column_label in column_labels]
 
     all_values = _reduce_spark_multi(
         kdf._sdf, [F.collect_set(kdf._internal.spark_column_for(label)) for label in column_labels]
