2c615ad076a262b4a3efaddca4eaa62415d6edb2
박현우 <cleanby@naver.com>
2020-05-12 10:18:04 +0900

    Add test for Series.to_frame() with  parameter (#1479)
    Adding missing test for Series.to_frame() with parameter `name`
    
    <img width="455" alt="스크린샷 2020-05-12 오전 12 56 46" src="https://user-images.githubusercontent.com/7010554/81582892-88cdae80-93eb-11ea-81c8-f39608e03eb9.png">

--- databricks/koalas/tests/test_series.py
+++ databricks/koalas/tests/test_series.py
@@ -1489,3 +1489,16 @@ class SeriesTest(ReusedSQLTestCase, SQLTestUtils):
         kser = ks.from_pandas(pser)
 
         self.assert_eq(repr(pser.floordiv(np.nan)), repr(kser.floordiv(np.nan)))
+
+    def test_to_frame(self):
+        kser = ks.Series(["a", "b", "c"])
+        pser = kser.to_pandas()
+
+        self.assert_eq(repr(pser.to_frame(name="a")), repr(kser.to_frame(name="a")))
+
+        # for MultiIndex
+        midx = pd.MultiIndex.from_tuples([("a", "x"), ("b", "y"), ("c", "z")])
+        kser = ks.Series(["a", "b", "c"], index=midx)
+        pser = kser.to_pandas()
+
+        self.assert_eq(repr(pser.to_frame(name="a")), repr(kser.to_frame(name="a")))
