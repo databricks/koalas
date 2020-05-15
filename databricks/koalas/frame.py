c2477524c9034076c854c2feb43777d0a9cf15bd
hyunwoo <cleanby@naver.com>
2020-05-15 09:29:08 +0900

    recover written note

--- databricks/koalas/frame.py
+++ databricks/koalas/frame.py
@@ -1768,6 +1768,9 @@ defaultdict(<class 'list'>, {'col..., 'col...})]
         """
         Print DataFrame in Markdown-friendly format.
 
+        .. note:: This method should only be used if the resulting Pandas object is expected
+                  to be small, as all the data is loaded into the driver's memory.
+
         Parameters
         ----------
         buf : writable buffer, defaults to sys.stdout
