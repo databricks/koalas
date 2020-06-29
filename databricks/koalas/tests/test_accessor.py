6a2a715d32392a5cc471a2e3573ca14d6d801240
Sam Cook <scook@esri.com>
2020-06-29 18:06:05 -0500

    Reorg imports and add file header

--- databricks/koalas/tests/test_accessor.py
+++ databricks/koalas/tests/test_accessor.py
@@ -1,9 +1,26 @@
-import warnings
-import unittest
+#
+# Copyright (C) 2019 Databricks, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 import contextlib
+import unittest
+import warnings
+
 import numpy as np
 import pandas as pd
-import databricks.koalas as ks
+
+from databricks import koalas as ks
 from databricks.koalas.testing.utils import assert_produces_warning
 from databricks.koalas.extensions import (
     register_dataframe_accessor,
