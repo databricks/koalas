66e53d84d45816686ca168c1440bd15b99dda93a
Thomas Spura <thomas.spura@gmail.com>
2020-08-13 21:52:25 +0200

    Remove SeriesGroupBy.cumcount from missing functions

--- databricks/koalas/missing/groupby.py
+++ databricks/koalas/missing/groupby.py
@@ -94,7 +94,6 @@ class MissingPandasLikeSeriesGroupBy(object):
     # Functions
     agg = _unsupported_function("agg")
     aggregate = _unsupported_function("aggregate")
-    cumcount = _unsupported_function("cumcount")
     describe = _unsupported_function("describe")
     get_group = _unsupported_function("get_group")
     median = _unsupported_function("median")
