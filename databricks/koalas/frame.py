1a8b354814fe3d811fd927d556a14b9ad650a1cb
itholic <haejoon309@naver.com>
2020-09-17 01:53:17 +0900

    Fix doctest

--- databricks/koalas/frame.py
+++ databricks/koalas/frame.py
@@ -10203,7 +10203,7 @@ defaultdict(<class 'list'>, {'col..., 'col...})]
         Examples
         --------
         >>> data = {'col_1': [3, 2, 1, 0], 'col_2': ['a', 'b', 'c', 'd']}
-        >>> pd.DataFrame.from_dict(data)
+        >>> ks.DataFrame.from_dict(data)
            col_1 col_2
         0      3     a
         1      2     b
