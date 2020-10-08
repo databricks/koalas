7b819730f8121ee9b60cd6d2093ae93d240bc984
Xinrong Meng <xinrong.meng@databricks.com>
2020-10-08 14:42:01 -0700

    Explicit error message

--- databricks/koalas/base.py
+++ databricks/koalas/base.py
@@ -254,7 +254,7 @@ class IndexOpsMixin(object, metaclass=ABCMeta):
                 return column_op(SF.repeat)(self, other)
             else:
                 raise TypeError(
-                    "string multiplication can only be applied between a string series and an int series or literals."
+                    "multiplication involves a string series can only be applied to an int series or literal"
                 )
 
         return column_op(Column.__mul__)(self, other)
@@ -360,7 +360,7 @@ class IndexOpsMixin(object, metaclass=ABCMeta):
                 return column_op(SF.repeat)(self, other)
             else:
                 raise TypeError(
-                    "string multiplication can only be applied between a string series and an int series or literals."
+                    "multiplication involves a string series can only be applied to an int series or literal"
                 )
 
         return column_op(Column.__rmul__)(self, other)
