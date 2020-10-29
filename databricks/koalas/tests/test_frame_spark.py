f62f2f2302aff0d5f7f467713733648d8f01ad63
Xinrong Meng <xinrong.meng@databricks.com>
2020-10-29 10:40:50 -0700

    Adjust imports

--- databricks/koalas/tests/test_frame_spark.py
+++ databricks/koalas/tests/test_frame_spark.py
@@ -13,8 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import os
 from distutils.version import LooseVersion
+import os
 
 import pandas as pd
 import pyspark
