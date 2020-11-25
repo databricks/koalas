867df668a242d445e4ce8b6d64d44d5496364c2f
Takuya UESHIN <ueshin@databricks.com>
2020-11-25 15:14:12 -0800

    Fix.

--- databricks/koalas/series.py
+++ databricks/koalas/series.py
@@ -4955,9 +4955,7 @@ class Series(Frame, IndexOpsMixin, Generic[T]):
             return result if result is not None else np.nan
 
         # The data is expected to be small so it's fine to transpose/use default index.
-        with ks.option_context(
-            "compute.default_index_type", "distributed", "compute.max_rows", 1
-        ):
+        with ks.option_context("compute.default_index_type", "distributed", "compute.max_rows", 1):
             kdf = ks.DataFrame(sdf)  # type: DataFrame
             kdf.columns = pd.Index(where)
             return first_series(kdf.transpose()).rename(self.name)
