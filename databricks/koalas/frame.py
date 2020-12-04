5e54d485dc38b20191ffbc35b6ff8fb6404012c9
Xinrong Meng <xinrong.meng@databricks.com>
2020-12-03 16:36:34 -0800

    Note from transpose

--- databricks/koalas/frame.py
+++ databricks/koalas/frame.py
@@ -6463,6 +6463,21 @@ defaultdict(<class 'list'>, {'col..., 'col...})]
         -------
         DataFrame
 
+        .. note:: This method is based on an expensive operation due to the nature
+            of big data. Internally it needs to generate each row for each value, and
+            then group twice - it is a huge operation. To prevent misusage, this method
+            has the 'compute.max_rows' default limit of input length, and raises a ValueError.
+
+                >>> from databricks.koalas.config import option_context
+                >>> with option_context('compute.max_rows', 1000):  # doctest: +NORMALIZE_WHITESPACE
+                ...     ks.DataFrame({'a': range(1001)}).swapaxes()
+                Traceback (most recent call last):
+                  ...
+                ValueError: Current DataFrame has more then the given limit 1000 rows.
+                Please set 'compute.max_rows' by using 'databricks.koalas.config.set_option'
+                to retrieve to retrieve more than 1000 rows. Note that, before changing the
+                'compute.max_rows', this operation is considerably expensive.
+
         Examples
         --------
         >>> kdf = ks.DataFrame(
