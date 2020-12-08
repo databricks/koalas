27df1a53087700615540457f07976a00b379501d
Xinrong Meng <xinrong.meng@databricks.com>
2020-12-08 13:39:33 -0800

    Add note for transpose

--- databricks/koalas/frame.py
+++ databricks/koalas/frame.py
@@ -4017,6 +4017,25 @@ defaultdict(<class 'list'>, {'col..., 'col...})]
 
         It can also be called using ``self @ other`` in Python >= 3.5.
 
+        .. note:: This method is based on an expensive operation due to the nature
+            of big data. Internally it needs to generate each row for each value, and
+            then group twice - it is a huge operation. To prevent misusage, this method
+            has the 'compute.max_rows' default limit of input length, and raises a ValueError.
+
+                >>> from databricks.koalas.config import option_context
+                >>> with option_context(
+                ...     'compute.max_rows', 1000, "compute.ops_on_diff_frames", True
+                ... ):  # doctest: +NORMALIZE_WHITESPACE
+                ...     kdf = ks.DataFrame({'a': range(1001)})
+                ...     kser = ks.Series([2], index=['a'])
+                ...     kdf.dot(kser)
+                Traceback (most recent call last):
+                  ...
+                ValueError: Current DataFrame has more then the given limit 1000 rows.
+                Please set 'compute.max_rows' by using 'databricks.koalas.config.set_option'
+                to retrieve to retrieve more than 1000 rows. Note that, before changing the
+                'compute.max_rows', this operation is considerably expensive.
+
         Parameters
         ----------
         other : Series
@@ -4059,13 +4078,22 @@ defaultdict(<class 'list'>, {'col..., 'col...})]
         0   -4
         1    5
         dtype: int64
-
+        >>> kdf @ kser2
+        0   -4
+        1    5
+        dtype: int64
         >>> reset_option("compute.ops_on_diff_frames")
         """
         if not isinstance(other, ks.Series):
             raise TypeError("Unsupported type {}".format(type(other).__name__))
         else:
             return cast(ks.Series, other.dot(self.transpose())).rename(None)
+
+    def __matmul__(self, other):
+        """
+        Matrix multiplication using binary `@` operator in Python>=3.5.
+        """
+        return self.dot(other)
 
     def to_koalas(self, index_col: Optional[Union[str, List[str]]] = None) -> "DataFrame":
         """
