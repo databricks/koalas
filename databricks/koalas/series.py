e957025282d10d484cc1b0c0649bfee384de6617
Takuya UESHIN <ueshin@databricks.com>
2020-12-23 10:11:15 -0800

    Refine DataFrame/Series.product. (#1979)
    Refines `DataFrame/Series.product` to:
    
    - Consolidate and reuse `_reduce_for_stat_function`.
    - Support `axis`, `numeric_only`, and `min_count` parameters.
    - Enable to calculate values including negative values or zeros.

--- databricks/koalas/series.py
+++ databricks/koalas/series.py
@@ -36,7 +36,6 @@ import pyspark
 from pyspark import sql as spark
 from pyspark.sql import functions as F, Column
 from pyspark.sql.types import (
-    BooleanType,
     DoubleType,
     FloatType,
     IntegerType,
@@ -5285,61 +5284,6 @@ class Series(Frame, IndexOpsMixin, Generic[T]):
         dtype: int64
         """
         return first_series(self.to_frame().tail(n=n)).rename(self.name)
-
-    def product(self, min_count=0) -> Union[int, float]:
-        """
-        Return the product of the values.
-
-        .. note:: unlike pandas', Koalas' emulates product by ``exp(sum(log(...)))``
-            trick. Therefore, it only works for positive numbers.
-
-        Parameters
-        ----------
-        min_count : int, default 0
-            The required number of valid values to perform the operation. If fewer than
-            ``min_count`` non-NA values are present the result will be NA.
-
-        Examples
-        --------
-        >>> ks.Series([1, 2, 3, 4, 5]).prod()
-        120
-
-        By default, the product of an empty or all-NA Series is ``1``
-
-        >>> ks.Series([]).prod()
-        1.0
-
-        This can be controlled with the ``min_count`` parameter
-
-        >>> ks.Series([]).prod(min_count=1)
-        nan
-        """
-        # When number of valid values is fewer than `min_count`, pandas returns np.nan
-        if (min_count > 0) and (len(self.dropna()) < min_count):
-            return np.nan
-
-        data_type = self.spark.data_type
-        if isinstance(data_type, BooleanType):
-            return self.all()
-        elif isinstance(data_type, NumericType):
-            spark_frame = self._internal.spark_frame
-            spark_column = self.spark.column
-
-            cond = F.when(spark_column.isNull(), F.lit(1)).otherwise(spark_column)
-            spark_frame = spark_frame.select(F.exp(F.sum(F.log(cond))))
-
-            result = spark_frame.head(1)[0][0]
-            if result is None:
-                # When Series is empty, pandas returns 1.0
-                return 1.0
-            elif isinstance(data_type, IntegralType):
-                return int(round(result))
-            else:
-                return result
-        else:
-            raise TypeError("cannot perform prod with type {}".format(self.dtype))
-
-    prod = product
 
     def explode(self) -> "Series":
         """
