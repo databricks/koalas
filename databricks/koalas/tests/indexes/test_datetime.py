2e6094bccd244b650a07c08b519d979ad65a884f
xinrong-databricks <47337188+xinrong-databricks@users.noreply.github.com>
2021-03-04 19:40:34 -0800

    Implement DatetimeIndex.round(), DatetimeIndex.floor(), and DatetimeIndex.ceil() (#2082)
    ```py
            >>> rng = ks.date_range('1/1/2018 11:59:00', periods=3, freq='min')
    
            >>> rng
            DatetimeIndex(['2018-01-01 11:59:00', '2018-01-01 12:00:00',
                           '2018-01-01 12:01:00'],
                          dtype='datetime64[ns]', freq=None)
    
            >>> rng.round("H")
            DatetimeIndex(['2018-01-01 12:00:00', '2018-01-01 12:00:00',
                           '2018-01-01 12:00:00'],
                          dtype='datetime64[ns]', freq=None)
    
            >>> rng.floor("H")
            DatetimeIndex(['2018-01-01 11:00:00', '2018-01-01 12:00:00',
                           '2018-01-01 12:00:00'],
                          dtype='datetime64[ns]', freq=None)
    
            >>> rng.ceil('H')
            DatetimeIndex(['2018-01-01 12:00:00', '2018-01-01 12:00:00',
                           '2018-01-01 13:00:00'],
                          dtype='datetime64[ns]', freq=None)
    ```

--- databricks/koalas/tests/indexes/test_datetime.py
+++ databricks/koalas/tests/indexes/test_datetime.py
@@ -57,6 +57,10 @@ class DatetimeIndexTest(ReusedSQLTestCase, TestUtils):
     def idx_pairs(self):
         return list(zip(self.kidxs, self.pidxs))
 
+    def _disallow_nanoseconds(self, f):
+        self.assertRaises(ValueError, lambda: f(freq="ns"))
+        self.assertRaises(ValueError, lambda: f(freq="N"))
+
     def test_properties(self):
         for kidx, pidx in self.idx_pairs:
             self.assert_eq(kidx.year, pidx.year)
@@ -85,3 +89,24 @@ class DatetimeIndexTest(ReusedSQLTestCase, TestUtils):
             if LooseVersion(pd.__version__) >= LooseVersion("1.2.0"):
                 self.assert_eq(kidx.day_of_year, pidx.day_of_year)
                 self.assert_eq(kidx.day_of_week, pidx.day_of_week)
+
+    def test_ceil(self):
+        for kidx, pidx in self.idx_pairs:
+            for freq in self.fixed_freqs:
+                self.assert_eq(kidx.ceil(freq), pidx.ceil(freq))
+
+        self._disallow_nanoseconds(self.kidxs[0].ceil)
+
+    def test_floor(self):
+        for kidx, pidx in self.idx_pairs:
+            for freq in self.fixed_freqs:
+                self.assert_eq(kidx.floor(freq), pidx.floor(freq))
+
+        self._disallow_nanoseconds(self.kidxs[0].floor)
+
+    def test_round(self):
+        for kidx, pidx in self.idx_pairs:
+            for freq in self.fixed_freqs:
+                self.assert_eq(kidx.round(freq), pidx.round(freq))
+
+        self._disallow_nanoseconds(self.kidxs[0].round)
