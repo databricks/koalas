e8d4ff4f1b4ee36eac16cff8b12e7c0d03b12028
Xinrong Meng <xinrong.meng@databricks.com>
2021-03-24 17:32:17 -0700

    New way; more tests

--- databricks/koalas/tests/test_dataframe.py
+++ databricks/koalas/tests/test_dataframe.py
@@ -5439,7 +5439,24 @@ class DataFrameTest(ReusedSQLTestCase, SQLTestUtils):
             kdf.between_time("0:15", "0:45").sort_index(),
         )
 
+        # Column label is 'index'
         pdf.columns = pd.Index(["index"])
+        kdf = ks.from_pandas(pdf)
+        self.assert_eq(
+            pdf.between_time("0:15", "0:45").sort_index(),
+            kdf.between_time("0:15", "0:45").sort_index(),
+        )
+
+        # Both index name and column label are 'index'
+        pdf.index.name = "index"
+        kdf = ks.from_pandas(pdf)
+        self.assert_eq(
+            pdf.between_time("0:15", "0:45").sort_index(),
+            kdf.between_time("0:15", "0:45").sort_index(),
+        )
+
+        # Index name is 'index', column label is ('X', 'A')
+        pdf.columns = pd.MultiIndex.from_arrays([["X"], ["A"]])
         kdf = ks.from_pandas(pdf)
         self.assert_eq(
             pdf.between_time("0:15", "0:45").sort_index(),
