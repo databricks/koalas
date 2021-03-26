33ce4be49ff85cc2a0199a67db3f8539d2cb1b1a
Xinrong Meng <xinrong.meng@databricks.com>
2021-03-26 10:40:31 -0700

    Reformat

--- databricks/koalas/tests/test_dataframe.py
+++ databricks/koalas/tests/test_dataframe.py
@@ -5519,7 +5519,9 @@ class DataFrameTest(ReusedSQLTestCase, SQLTestUtils):
 
         if LooseVersion(pd.__version__) >= LooseVersion("0.24"):
             # axis parameter is new in pandas version 0.24.0.
-            with self.assertRaisesRegex(NotImplementedError, "at_time currently only works for axis=0"):
+            with self.assertRaisesRegex(
+                NotImplementedError, "at_time currently only works for axis=0"
+            ):
                 kdf.at_time("0:15", axis=1)
 
         kdf = ks.DataFrame({"A": [1, 2, 3, 4]})
